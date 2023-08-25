f2c56521dc3f8d53b2f7141283b8d1b13824f8eb
Matthias Fabry <matthias.fabry@kuleuven.be>
2023-08-25 16:56:11 +0200

    use FunctionWrappers (#30)

--- src/Evolution/Equations.jl
+++ src/Evolution/Equations.jl
@@ -1,91 +1,85 @@
-function equationHSE!(results::Matrix{TT}, sm::StellarModel, k::Int, i::Int,
-                      varm1::AbstractVector{TT}, var00::AbstractVector{TT}, varp1::AbstractVector{TT},
+function equationHSE(sm::StellarModel, k::Int,
+                      varm1::Matrix{TT}, var00::Matrix{TT}, varp1::Matrix{TT},
                       eosm1::EOSResults{TT}, eos00::EOSResults{TT}, eosp1::EOSResults{TT},
-                      κm1::TT, κ00::TT, κp1::TT) where {TT<:Real}
+                      κm1::TT, κ00::TT, κp1::TT)::TT where {TT<:Real}
     if k == sm.nz  # atmosphere boundary condition
-        lnP₀ = var00[sm.vari[:lnP]]
-        r₀ = exp(var00[sm.vari[:lnr]])
+        lnP₀ = var00[k, sm.vari[:lnP]]
+        r₀ = exp(var00[k, sm.vari[:lnr]])
         g₀ = CGRAV * sm.mstar / r₀^2
-        results[k, i] = lnP₀ - log(2g₀ / (3κ00))  # Eddington gray, ignoring radiation pressure term
-        return
+        return lnP₀ - log(2g₀ / (3κ00))  # Eddington gray, ignoring radiation pressure term
     end
-    lnP₊ = varp1[sm.vari[:lnP]]
-    lnP₀ = var00[sm.vari[:lnP]]
+    lnP₊ = varp1[k, sm.vari[:lnP]]
+    lnP₀ = var00[k, sm.vari[:lnP]]
     lnPface = (sm.dm[k] * lnP₀ + sm.dm[k + 1] * lnP₊) / (sm.dm[k] + sm.dm[k + 1])
-    r₀ = exp(var00[sm.vari[:lnr]])
+    r₀ = exp(var00[k, sm.vari[:lnr]])
     dm = (sm.m[k + 1] - sm.m[k])
 
-    results[k, i] = (exp(lnPface) * (lnP₊ - lnP₀) / dm + CGRAV * sm.m[k] / (4π * r₀^4)) /
+    return (exp(lnPface) * (lnP₊ - lnP₀) / dm + CGRAV * sm.m[k] / (4π * r₀^4)) /
                 (CGRAV * sm.m[k] / (4π * r₀^4))
-    return
 end
 
-function equationT!(results::Matrix{TT}, sm::StellarModel, k::Int, i::Int,
-                    varm1::AbstractVector{TT}, var00::AbstractVector{TT}, varp1::AbstractVector{TT},
+function equationT(sm::StellarModel, k::Int,
+                    varm1::Matrix{TT}, var00::Matrix{TT}, varp1::Matrix{TT},
                     eosm1::EOSResults{TT}, eos00::EOSResults{TT}, eosp1::EOSResults{TT},
-                    κm1::TT, κ00::TT, κp1::TT) where {TT<:Real}
+                    κm1::TT, κ00::TT, κp1::TT)::TT where {TT<:Real}
     if k == sm.nz  # atmosphere boundary condition
-        lnT₀ = var00[sm.vari[:lnT]]
-        L₀ = var00[sm.vari[:lum]] * LSUN
-        r₀ = exp(var00[sm.vari[:lnr]])
-        results[k, i] = lnT₀ - log(L₀ / (BOLTZ_SIGMA * 4π * r₀^2)) / 4  # Eddington gray, ignoring radiation pressure term
-        return
+        lnT₀ = var00[k, sm.vari[:lnT]]
+        L₀ = var00[k, sm.vari[:lum]] * LSUN
+        r₀ = exp(var00[k, sm.vari[:lnr]])
+        return lnT₀ - log(L₀ / (BOLTZ_SIGMA * 4π * r₀^2)) / 4  # Eddington gray, ignoring radiation pressure term
     end
     κface = exp((sm.dm[k] * log(κ00) + sm.dm[k + 1] * log(κp1)) / (sm.dm[k] + sm.dm[k + 1]))
-    L₀ = var00[sm.vari[:lum]] * LSUN
-    r₀ = exp(var00[sm.vari[:lnr]])
-    Pface = exp((sm.dm[k] * var00[sm.vari[:lnP]] + sm.dm[k + 1] * varp1[sm.vari[:lnP]]) /
+    L₀ = var00[k, sm.vari[:lum]] * LSUN
+    r₀ = exp(var00[k, sm.vari[:lnr]])
+    Pface = exp((sm.dm[k] * var00[k, sm.vari[:lnP]] + sm.dm[k + 1] * varp1[k, sm.vari[:lnP]]) /
                 (sm.dm[k] + sm.dm[k + 1]))
-    lnT₊ = varp1[sm.vari[:lnT]]
-    lnT₀ = var00[sm.vari[:lnT]]
+    lnT₊ = varp1[k, sm.vari[:lnT]]
+    lnT₀ = var00[k, sm.vari[:lnT]]
     Tface = exp((sm.dm[k] * lnT₀ + sm.dm[k + 1] * lnT₊) / (sm.dm[k] + sm.dm[k + 1]))
 
     ∇ᵣ = 3κface * L₀ * Pface / (16π * CRAD * CLIGHT * CGRAV * sm.m[k] * Tface^4)
     ∇ₐ = (sm.dm[k] * eos00.∇ₐ + sm.dm[k + 1] * eosp1.∇ₐ) / (sm.dm[k] + sm.dm[k + 1])
 
     if (∇ᵣ < ∇ₐ)
-        results[k, i] = (Tface * (lnT₊ - lnT₀) / sm.dm[k] +
+        return (Tface * (lnT₊ - lnT₀) / sm.dm[k] +
                      CGRAV * sm.m[k] * Tface / (4π * r₀^4 * Pface) * ∇ᵣ) /
                     (CGRAV * sm.m[k] * Tface / (4π * r₀^4 * Pface))  # only radiative transport
     else  # should do convection here
-        results[k, i] = (Tface * (lnT₊ - lnT₀) / sm.dm[k] +
+        return (Tface * (lnT₊ - lnT₀) / sm.dm[k] +
                      CGRAV * sm.m[k] * Tface / (4π * r₀^4 * Pface) * ∇ₐ) /
                     (CGRAV * sm.m[k] * Tface / (4π * r₀^4 * Pface))  # only radiative transport
     end
-    return
 end
 
-function equationLuminosity!(results::Matrix{TT}, sm::StellarModel, k::Int, i::Int,
-                             varm1::AbstractVector{TT}, var00::AbstractVector{TT}, varp1::AbstractVector{TT},
+function equationLuminosity( sm::StellarModel, k::Int,
+                             varm1::Matrix{TT}, var00::Matrix{TT}, varp1::Matrix{TT},
                              eosm1::EOSResults{TT}, eos00::EOSResults{TT}, eosp1::EOSResults{TT},
-                             κm1::TT, κ00::TT, κp1::TT) where {TT<:Real}
+                             κm1::TT, κ00::TT, κp1::TT)::TT where {TT<:Real}
     if k > 1
-        L₋ = varm1[sm.vari[:lum]] * LSUN  # change it if not at first cell
+        L₋ = varm1[k,sm.vari[:lum]] * LSUN  # change it if not at first cell
     else
         L₋::TT = 0  # central luminosity is zero at first cell
     end
-    L₀ = var00[sm.vari[:lum]] * LSUN
+    L₀ = var00[k, sm.vari[:lum]] * LSUN
     ρ₀ = eos00.ρ
     cₚ = eos00.cₚ
     δ = eos00.δ
-    dTdt = (exp(var00[sm.vari[:lnT]]) - exp(sm.ssi.lnT[k])) / sm.ssi.dt
-    dPdt = (exp(var00[sm.vari[:lnP]]) - exp(sm.ssi.lnP[k])) / sm.ssi.dt
+    dTdt = (exp(var00[k, sm.vari[:lnT]]) - exp(sm.ssi.lnT[k])) / sm.ssi.dt
+    dPdt = (exp(var00[k, sm.vari[:lnP]]) - exp(sm.ssi.lnP[k])) / sm.ssi.dt
 
-    ϵnuc = 0.1 * var00[sm.vari[:H1]]^2 * ρ₀ * (exp(var00[sm.vari[:lnT]]) / 1e6)^4 +
-           0.1 * var00[sm.vari[:H1]] * ρ₀ * (exp(var00[sm.vari[:lnT]]) / 1e7)^18
-
-    results[k, i] = ((L₀ - L₋) / sm.dm[k] - ϵnuc + cₚ * dTdt - (δ / ρ₀) * dPdt)  # no neutrinos
-    return
+    ϵnuc = 0.1 * var00[k, sm.vari[:H1]]^2 * ρ₀ * (exp(var00[k, sm.vari[:lnT]]) / 1e6)^4 +
+           0.1 * var00[k, sm.vari[:H1]] * ρ₀ * (exp(var00[k, sm.vari[:lnT]]) / 1e7)^18
+    return ((L₀ - L₋) / sm.dm[k] - ϵnuc + cₚ * dTdt - (δ / ρ₀) * dPdt)  # no neutrinos
 end
 
-function equationContinuity!(results::Matrix{TT}, sm::StellarModel, k::Int, i::Int,
-                             varm1::AbstractVector{TT}, var00::AbstractVector{TT}, varp1::AbstractVector{TT},
+function equationContinuity(sm::StellarModel, k::Int,
+                             varm1::Matrix{TT}, var00::Matrix{TT}, varp1::Matrix{TT},
                              eosm1::EOSResults{TT}, eos00::EOSResults{TT}, eosp1::EOSResults{TT},
-                             κm1::TT, κ00::TT, κp1::TT) where {TT<:Real}
+                             κm1::TT, κ00::TT, κp1::TT)::TT where {TT<:Real}
     ρ₀ = eos00.ρ
-    r₀ = exp(var00[sm.vari[:lnr]])
+    r₀ = exp(var00[k, sm.vari[:lnr]])
     if k > 1
-        r₋ = exp(varm1[sm.vari[:lnr]])  # change it if not at first cell
+        r₋ = exp(varm1[k,sm.vari[:lnr]])  # change it if not at first cell
     else
         r₋::TT = 0  # central radius is zero at first cell
     end
@@ -99,33 +93,30 @@ function equationContinuity!(results::Matrix{TT}, sm::StellarModel, k::Int, i::I
     expected_dr³_dm = 3 / (4π * ρ₀)
     actual_dr³_dm = (r₀^3 - r₋^3) / dm
 
-    results[k, i] = (expected_dr³_dm - actual_dr³_dm) * ρ₀
-    return
+    return (expected_dr³_dm - actual_dr³_dm) * ρ₀
 end
 
 # To test performance, include 8 isotopes similar to basic.net in MESA.
 # of course we are keeping these fixed now, but it lets us test their impact on the
 # computation of the jacobian
-function equationH1!(results::Matrix{TT}, sm::StellarModel, k::Int, i::Int,
-                     varm1::AbstractVector{TT}, var00::AbstractVector{TT}, varp1::AbstractVector{TT},
+function equationH1(sm::StellarModel, k::Int,
+                     varm1::Matrix{TT}, var00::Matrix{TT}, varp1::Matrix{TT},
                      eosm1::EOSResults{TT}, eos00::EOSResults{TT}, eosp1::EOSResults{TT},
-                     κm1::TT, κ00::TT, κp1::TT) where {TT<:Real}
+                     κm1::TT, κ00::TT, κp1::TT)::TT where {TT<:Real}
     ρ₀ = eos00.ρ
-    ϵnuc = 0.1 * var00[sm.vari[:H1]]^2 * ρ₀ * (exp(var00[sm.vari[:lnT]]) / 1e6)^4 +
-           0.1 * var00[sm.vari[:H1]] * ρ₀ * (exp(var00[sm.vari[:lnT]]) / 1e7)^18
+    ϵnuc = 0.1 * var00[k, sm.vari[:H1]]^2 * ρ₀ * (exp(var00[k, sm.vari[:lnT]]) / 1e6)^4 +
+           0.1 * var00[k, sm.vari[:H1]] * ρ₀ * (exp(var00[k, sm.vari[:lnT]]) / 1e7)^18
     rate_per_unit_mass = 4 * ϵnuc / ((4 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He4].mass) * AMU * CLIGHT^2)
 
     Xi = sm.ssi.ind_vars[(k - 1) * sm.nvars + sm.vari[:H1]]
 
-    results[k, i] = (var00[sm.vari[:H1]] - Xi) / sm.ssi.dt +
+    return (var00[k, sm.vari[:H1]] - Xi) / sm.ssi.dt +
                 Chem.isotope_list[:H1].mass * AMU * rate_per_unit_mass
-    return
 end
 
-function equationHe4!(results::Matrix{TT}, sm::StellarModel, k::Int, i::Int,
-                      varm1::AbstractVector{TT}, var00::AbstractVector{TT}, varp1::AbstractVector{TT},
+function equationHe4(sm::StellarModel, k::Int,
+                      varm1::Matrix{TT}, var00::Matrix{TT}, varp1::Matrix{TT},
                       eosm1::EOSResults{TT}, eos00::EOSResults{TT}, eosp1::EOSResults{TT},
-                      κm1::TT, κ00::TT, κp1::TT) where {TT<:Real}
-    results[k, i] = var00[sm.vari[:He4]] + var00[sm.vari[:H1]] - 1.0
-    return
+                      κm1::TT, κ00::TT, κp1::TT)::TT where {TT<:Real}
+    return var00[k, sm.vari[:He4]] + var00[k, sm.vari[:H1]] - 1.0
 end
