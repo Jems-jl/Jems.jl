c5d5852d91c8125ae15b404631e9d702dbcef2e5
Matthias Fabry <matthias.fabry@kuleuven.be>
2023-09-01 20:49:14 +0000

    general definitions of in EOSresults

--- src/Opacity/Opacity.jl
+++ src/Opacity/Opacity.jl
@@ -4,6 +4,13 @@ using ..Chem, ..Constants
 
 export AbstractOpacity
 
+"""
+    abstract type AbstractOpacity
+
+Abstract supertype from which all defined opacity laws must derive, _ie_:
+
+`struct MyOpacity <: AbstractOpacity`
+"""
 abstract type AbstractOpacity end
 
 include("SimpleElectronScattering.jl")
