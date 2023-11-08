73ddebb309e5b421bd7cad6c5eef7023f671980a
Digonto Rahman <digonto.rahman@student.kuleuven.be>
2023-11-08 15:05:54 +0100

    working LaneEmden equation

---
+++ examples/NuclearBurning copy.jl
@@ -0,0 +1,248 @@
+#=
+# NuclearBurning.jl
+
+This notebook provides a simple example of a star with simplified mycrophysics undergoing nuclear burning.
+Import all necessary Jems modules. We will also do some benchmarks, so we import BenchmarkTools as well.
+=#
+using BenchmarkTools
+using Jems.Chem
+using Jems.Constants
+using Jems.EOS
+using Jems.Opacity
+using Jems.NuclearNetworks
+using Jems.StellarModels
+using Jems.Evolution
+
+##
+#=
+### Model creation
+
+We start by creating the stellar model. In this example we consider a model with 6 independent variables, two of which
+correspond to composition. The independent variables here are $\ln(P)$, $\ln(T)$, $\ln(r)$, the luminosity $L$ and the
+mass fractions of Hydrogen and Helium.
+
+The Evolution module has pre-defined equations corresponding to these variables, which we provide here. For now, only a
+simple (fully ionized) ideal gas law EOS is available. Similarly, only a simple simple electron scattering opacity equal
+to $\kappa=0.2(1+X)\;[\mathrm{cm^2\;g^{-1}}]$ is available.
+=#
+nvars = 6
+nspecies = 2
+varnames = [:lnP, :lnT, :lnr, :lum, :H1, :He4]
+structure_equations = [Evolution.equationHSE, Evolution.equationT,
+                       Evolution.equationContinuity, Evolution.equationLuminosity]
+net = NuclearNetwork([:H1,:He4], [(:toy_rates, :toy_pp), (:toy_rates, :toy_cno)])
+nz = 1000
+nextra = 200
+eos = EOS.IdealEOS(false)
+opacity = Opacity.SimpleElectronScatteringOpacity()
+sm = StellarModel(varnames, structure_equations, nvars, nspecies, nz, nextra, net, eos, opacity);
+
+##
+#=
+### Initialize StellarModel and evaluate equations and jacobian
+
+We do not have a working initial condition yet. We require pressure, temperature profiles. One simple available initial
+condition is that of an n=1 polytrope. This sets the pressure and density and computes the temperature from the EOS. The
+luminosity is initialized by assuming pure radiative transport for the temperature gradient produced by the polytrope.
+
+The normal evolution loop will store the information at the end of the step into an attribute of type `StellarStepInfo`,
+stored at `sm.esi` (_end step info_). After initializing our polytrope we can mimic that behavior by calling 
+`set_end_step_info!(sm)`. We then 'cycle' this info into the information of a hypothetical previous step with
+`cycle_step_info`, so now `sm.psi` contains our initial condition. Finally we call `set_start_step_info` to use `sm.psi`
+(_previous step info_) to populate the information needed before the Newton solver in `sm.ssi` (_start step info_).
+At last we are in position to evaluate the equations and compute the Jacobian.
+=#
+#change to new function
+StellarModels.n1_polytrope_initial_condition!(sm, MSUN, 100 * RSUN; initial_dt=10 * SECYEAR)
+Evolution.set_end_step_info!(sm)
+Evolution.cycle_step_info!(sm);
+Evolution.set_start_step_info!(sm)
+Evolution.eval_jacobian_eqs!(sm)
+
+##
+#=
+### Benchmarking
+
+The previous code leaves everything ready to solve the linearized system. 
+For now we make use of a the serial Thomas algorithm for tridiagonal block matrices.
+We first show how long it takes to evaluate one row (meaning, one set of lower, diagonal
+and upper block) of the Jacobian matrix.
+=#
+@benchmark Evolution.eval_jacobian_eqs_row!(sm, 2)
+
+##
+#=
+On my machine, this takes $\sim 3\;\mathrm{\mu s}$. This is a short amount of time, but we have a thousand cells
+to compute. Let's benchmark the calculation of the full jacobian.
+=#
+@benchmark Evolution.eval_jacobian_eqs!(sm)
+
+##
+#=
+And on my computer, this took about $1\;\mathrm{ms}$. Even though we have a thousand cells, the computation time was
+not a thousand times longer than computing the components of the jacobian for a single cell. The reason for this is that
+the calculation is parallelized so cells are done independently. However, I used 8 cores for my calculations, so the
+scaling is less than ideal. One of the main culprits here is the garbage collector. Current versions of julia can only
+perform garbage collection in a serial way, so it does not take advantage of all threads. Starting with julia 1.10, the
+garbage collector will be able to run in multiple threads, so that should alleviate issues with performance scaling.
+
+To get an idea of how much a complete iteration of the solver takes, we need to benchmark
+both the calculation of the Jacobian and the matrix solver. This is because the matrix solver
+is destructive, as it uses the allocated Jacobian to store intermediate results. The time it takes
+to run the only the matrix solver can be determined by substracting the previous benchmark to this one.
+=#
+
+@benchmark begin
+    Evolution.eval_jacobian_eqs!($sm)
+    Evolution.thomas_algorithm!($sm)
+end
+##
+#=
+### Evolving our model
+
+We can now evolve our star! We will initiate a $1M_\odot$ star with a radius of $100R_\odot$ using an n=1 polytrope (it
+would be much better to use n=3 or n=3/2 polytropes, for now I only use this because there is a simple analytical
+solution). The star is expected to contract until it ignites hydrogen. We set a few options for the simulation with a
+toml file, which we generate dynamically. These simulation should complete in about a thousand steps once it reaches the
+`max_center_T` limit.
+
+Output is stored in HDF5 files, and easy to use functions are provided with the Evolution module to turn these HDF5
+files into DataFrame objects. HDF5 output is compressed by default.
+=#
+open("example_options.toml", "w") do file
+    write(file,
+          """
+          [solver]
+          newton_max_iter_first_step = 1000
+          newton_max_iter = 200
+
+          [timestep]
+          dt_max_increase = 2.0
+
+          [termination]
+          max_model_number = 1100
+          max_center_T = 4e7
+
+          [io]
+          profile_interval = 50
+          """)
+end
+StellarModels.set_options!(sm.opt, "./example_options.toml")
+rm(sm.opt.io.hdf5_history_filename; force=true)
+rm(sm.opt.io.hdf5_profile_filename; force=true)
+#change to new function
+StellarModels.n1_polytrope_initial_condition!(sm, MSUN, 100 * RSUN; initial_dt=1000 * SECYEAR)
+
+@time Evolution.do_evolution_loop(sm)
+
+##
+#=
+### Plotting with Makie
+
+Now that our simulation is complete we can analyze the results. We make use of the Makie package for this. I'm not a fan
+of the Makie defaults, so I adjust them. I normally also adjust the fonts to be consistent with \LaTeX, but I avoid that
+here so we don't need to distribute those fonts together with Jems.
+=#
+using CairoMakie, LaTeXStrings, MathTeXEngine
+basic_theme = Theme(
+                    fonts = (regular = texfont(:text), bold = texfont(:bold),
+                    italic = texfont(:italic), bold_italic = texfont(:bolditalic)),
+                    fontsize=30, resolution=(1000, 750), linewidth=7,
+                    Axis=(xlabelsize=40, ylabelsize=40, titlesize=40, xgridvisible=false, ygridvisible=false,
+                          spinewidth=2.5, xminorticksvisible=true, yminorticksvisible=true, xtickalign=1, ytickalign=1,
+                          xminortickalign=1, yminortickalign=1, xticksize=14, xtickwidth=2.5, yticksize=14,
+                          ytickwidth=2.5, xminorticksize=7, xminortickwidth=2.5, yminorticksize=7, yminortickwidth=2.5,
+                          xticklabelsize=35, yticklabelsize=35, xticksmirrored=true, yticksmirrored=true),
+                    Legend=(patchsize=(70, 10), framevisible=false, patchlabelgap=20, rowgap=10))
+set_theme!(basic_theme)
+
+##
+#=
+### Compare against polytropes
+
+Below we see how the profile of the star compares to different polytropes. We make use of the facility tools to obtain
+DataFrame objects out of the hdf5 output. In particular, `get_profile_names_from_hdf5` will provide the names of all 
+profiles contained within the hdf5 file, while `get_profile_dataframe_from_hdf5` is used to obtain one DataFrame
+corresponding to one stellar profile. The animation is constructed using the `Observable` type that makie provides. Note
+that the zero points of the polytropes are arbitrary.
+=#
+profile_names = StellarModels.get_profile_names_from_hdf5("profiles.hdf5")
+
+f = Figure();
+ax = Axis(f[1, 1]; xlabel=L"\log_{10}(\rho/\mathrm{[g\;cm^{-3}]})", ylabel=L"\log_{10}(P/\mathrm{[dyn]})")
+
+pname = Observable(profile_names[1])
+
+profile = @lift(StellarModels.get_profile_dataframe_from_hdf5("profiles.hdf5", $pname))
+log10_ρ = @lift($profile[!, "log10_ρ"])
+log10_P = @lift($profile[!, "log10_P"])
+
+profile_line = lines!(ax, log10_ρ, log10_P; label="real profile")
+xvals = LinRange(-13, 4, 100)
+lines!(ax, xvals, (1 + 1 / 1) .* xvals .+ 20; label="n=1")
+lines!(ax, xvals, (1 + 1 / (1.5)) .* xvals .+ 15; label="n=1.5")
+lines!(ax, xvals, (1 + 1 / 3) .* xvals .+ 15; label="n=3")
+axislegend(ax; position=:rb)
+
+model_number_str = @lift("model number=$(parse(Int,$pname))")
+profile_text = text!(ax, -10, 20; text=model_number_str)
+
+record(f, "rho_P_evolution.gif", profile_names[1:end]; framerate=2) do profile_name
+    pname[] = profile_name
+end
+
+# ![Movie polytrope](./rho_P_evolution.gif)
+
+##
+#=
+### Check nuclear burning
+
+We see that the structure evolves towards an n=3 polytrope. Deviations near the core are due to the non-homogeneous
+composition as hydrogen is burnt. We can similarly visualize how the hydrogen mass fraction changes in the simulation.
+In here only one frame shows the hydrogen that was burnt, to better visualize that you can adjust `profile_interval` in
+the [Io](Evolution.md##Io.jl) options (and probably adjust the framerate).
+=#
+profile_names = StellarModels.get_profile_names_from_hdf5("profiles.hdf5")
+
+f = Figure();
+ax = Axis(f[1, 1]; xlabel=L"\mathrm{Mass}\;[M_\odot]", ylabel=L"X")
+
+pname = Observable(profile_names[1])
+
+profile = @lift(StellarModels.get_profile_dataframe_from_hdf5("profiles.hdf5", $pname))
+mass = @lift($profile[!, "mass"])
+X = @lift($profile[!, "X"])
+model_number_str = @lift("model number=$(parse(Int,$pname))")
+
+profile_line = lines!(ax, mass, X; label="real profile")
+profile_text = text!(ax, 0.7, 0.0; text=model_number_str)
+
+record(f, "X_evolution.gif", profile_names[1:end]; framerate=2) do profile_name
+    pname[] = profile_name
+end
+
+# ![Movie polytrope](./X_evolution.gif)
+
+##
+#=
+### Plot a funny HR diagram
+
+Finally, we can also access the history data of the simulation. We use this to plot a simple HR diagram. As our
+microphysics are very simplistic, and the initial condition is not very physical, this looks a bit funny!
+=#
+f = Figure();
+ax = Axis(f[1, 1]; xlabel=L"\log_{10}(T_\mathrm{eff}/[K])", ylabel=L"\log_{10}(L/L_\odot)", xreversed=true)
+history = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+lines!(ax, log10.(history[!, "T_surf"]), log10.(history[!, "L_surf"]))
+f
+
+##
+#=
+### Perform some cleanup
+
+Internally we want to prevent storing any of the hdf5 files into our git repos, so I remove them. You can also take
+advantage of `julia` as a scripting language to post-process your simulation output in a similar way.
+=#
+rm("history.hdf5")
+rm("profiles.hdf5")
+rm("example_options.toml")
--- src/StellarModels/InitialCondition.jl
+++ src/StellarModels/InitialCondition.jl
@@ -122,3 +122,264 @@ function n1_polytrope_initial_condition!(sm::StellarModel, M::Real, R::Real; ini
     sm.dt = initial_dt
     sm.model_number = 0
 end
+
+
+
+"""
+    n1_polytrope_initial_condition(sm::StellarModel, M::Real, R::Real; initial_dt=100 * SECYEAR)
+
+Initializes a stellar model `sm` with values corresponding to an n=1 polytrope, setting the independent variables
+`sm.ind_vars`, etc. accordingly. Also sets the initial timestep to be taken, `initial_dt`.
+"""
+function n1_polytrope_initial_condition!(sm::StellarModel, M::Real, R::Real; initial_dt=100 * SECYEAR)
+    logdqs = get_logdq.(1:(sm.nz), sm.nz, -3.0, 0.0, 100)
+    dqs = 10 .^ logdqs
+    dqs = dqs ./ sum(dqs)
+    dms = dqs .* M
+    m_face = cumsum(dms)
+    m_cell = cumsum(dms)
+    # correct m_center
+    for i = 1:(sm.nz)
+        if i == 1
+            m_cell[i] = 0
+        elseif i != sm.nz
+            m_cell[i] = m_cell[i] - 0.5 * dms[i]
+        end
+    end
+
+    n = 1  # n = 1 polytrope after all...
+    rn = R / π  # ξ is defined as r/rn, where rn^2=(n+1)Pc/(4π G ρc^2)
+
+    ρc = M / (4π * rn^3 * (-π^2 * ForwardDiff.derivative(θ_n, π)))
+    Pc = 4π * CGRAV * rn^2 * ρc^2 / (n + 1)
+
+    ξ_cell = zeros(sm.nz)
+    ξ_face = zeros(sm.nz)
+    function mfunc(ξ, m)
+        return m - 4π * rn^3 * ρc * (-(-sin(ξ) + cos(ξ) * ξ))
+    end
+
+    for i = 1:(sm.nz)
+        if i == 1
+            ξ_cell[i] = 0
+        elseif i == sm.nz
+            ξ_cell[i] = π
+        else
+            mfunc_anon = ξ -> mfunc(ξ, m_cell[i])
+            ξ_cell[i] = find_zero(mfunc_anon, (0, π), Bisection())
+        end
+        if i == sm.nz
+            ξ_face[i] = π
+        else
+            mfunc_anon = ξ -> mfunc(ξ, m_face[i])
+            ξ_face[i] = find_zero(mfunc_anon, (0, π), Bisection())
+        end
+    end
+
+    # set radii, pressure and temperature, assuming ideal gas without Prad
+    for i = 1:(sm.nz)
+        μ = 0.5
+        XH = 1.0
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnr]] = log(rn * ξ_face[i])
+        if i > 1
+            P = Pc * (θ_n(ξ_cell[i]))^(n + 1)
+            ρ = ρc * (θ_n(ξ_cell[i]))^(n)
+        else
+            P = Pc
+            ρ = ρc
+        end
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnP]] = log(P)
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnT]] = log(P * μ / (CGAS * ρ))
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:H1]] = 1.0
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:He4]] = 0
+    end
+
+    # set m and dm
+    sm.mstar = M
+    sm.dm = dms
+    sm.m = m_face
+
+    # set luminosity
+    for i = 1:(sm.nz - 1)
+        μ = 0.5
+        Pface = Pc * (θ_n(ξ_face[i]))^(n + 1)
+        ρface = ρc * (θ_n(ξ_face[i]))^(n)
+        Tface = Pface * μ / (CGAS * ρface)
+        dlnT = sm.ind_vars[(i) * sm.nvars + sm.vari[:lnT]] - sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnT]]
+        dlnP = sm.ind_vars[(i) * sm.nvars + sm.vari[:lnP]] - sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnP]]
+        κ = 0.2
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lum]] = (dlnT / dlnP) *
+                                                          (16π * CRAD * CLIGHT * CGRAV * m_face[i] * Tface^4) /
+                                                          (3κ * Pface * LSUN)
+    end
+
+    # special cases, just copy values at edges
+    sm.ind_vars[(sm.nz - 1) * sm.nvars + sm.vari[:lnP]] = sm.ind_vars[(sm.nz - 2) * sm.nvars + sm.vari[:lnP]]
+    sm.ind_vars[(sm.nz - 1) * sm.nvars + sm.vari[:lnT]] = sm.ind_vars[(sm.nz - 2) * sm.nvars + sm.vari[:lnT]]
+    sm.ind_vars[(sm.nz - 1) * sm.nvars + sm.vari[:lum]] = sm.ind_vars[(sm.nz - 2) * sm.nvars + sm.vari[:lum]]
+
+    sm.time = 0.0
+    sm.dt = initial_dt
+    sm.model_number = 0
+end
+
+###########################MY CODE ADDED BELOW##############################################
+
+dydx(x,y,z,n) = z #derivative dy/dx = z
+dzdx(x,y,z,n) = -y^n -2*z/x #derivative dz/dx = -y^n -2*z/x
+#approximation of y and z for small ξ
+y_smallx(x,n) = 1 - 1/6*x^2 + n/120*x^4 -n*(8*n-5)/1520*x^6
+z_smallx(x,n) = - 1/3*x + n/30*x^3 -3*n*(8*n-5)/760*x^5;
+
+function endOfLoop!(xvals::LinRange, yvals::Vector{Float64}, zvals::Vector{Float64}, endIndex::Int)
+    slope = (xvals[endIndex-1] - yvals[endIndex-2]) / (xvals[endIndex-1] - xvals[endIndex-2])
+    xlast = xvals[endIndex-1] - yvals[endIndex-1] / slope
+    newxvals = zeros(endIndex)
+    newxvals[1:endIndex-1] = xrange[1:endIndex-1]; newxvals[endIndex] = xlast
+    return (newxvals, push!(yrange[1:endIndex-1],0.0))
+end
+
+function RungeKutta(n::Int64)
+    Δx = 1e-4
+    nsteps = 200_000 #maximum number of steps
+    #initialize first value of y and z using series approximation
+    xvals = LinRange(Δx,nsteps*Δx,nsteps)
+    yvals = zeros(nsteps); zvals = zeros(nsteps)
+    yvals[1] = y_smallx(Δx,n); zvals[1] = z_smallx(Δx,n)
+    
+    for i in 2:nsteps
+        x = xvals[i-1]; y = yvals[i-1]; z = zvals[i-1]
+        k₁ = Δx*dydx(x,y,z,n); l₁ = Δx*dzdx(x,y,z,n)
+        ynew = y + k₁/2
+        if ynew < 0.0
+            xvals, yvals = endOfLoop!(xvals,yvals,zvals,i)
+            break
+        end
+        k₂ = Δx*dydx(x+Δx/2,ynew,z+l₁/2,n); l₂ = Δx*dzdx(x+Δx/2,ynew,z+l₁/2,n)
+        ynew = y+k₂/2
+        if ynew < 0.0
+            xvals, yvals = endOfLoop!(xvals,yvals,zvals,i)
+            break
+        end
+        k₃ = Δx*dydx(x+Δx/2,ynew,z+l₂/2,n); l₃ = Δx*dzdx(x+Δx/2,ynew,z+l₂/2,n)
+        ynew = y+k₃
+        if ynew < 0.0
+            xvals, yvals = endOfLoop!(xvals,yvals,zvals,i)
+            break
+        end
+        k₄ = Δx*dydx(x+Δx,ynew,z+l₃,n);l₄ = Δx*dzdx(x+Δx,ynew,z+l₃,n)
+        yvals[i] = y+k₁/6+k₂/3+k₃/3+k₄/6 #new y value
+        zvals[i] = z+l₁/6+l₂/3+l₃/3+l₄/6 #new z value
+    end
+    return xvals, yvals
+end
+
+function linear_interpolation(xvalues, yvalues)
+    function θ_n(x)
+        for i in eachindex(xvalues)
+            if xvalues[i] <= x < xvalues[i+1]
+                return yvalues[i] + (yvalues[i+1] - yvalues[i]) / (xvalues[i+1] - xvalues[i]) * (x - xvalues[i])
+            end
+        end
+    end
+    return θ_n
+end
+
+function get_θn_and_ξ1(n)
+    xvals, yvals = RungeKutta(n)
+    return (linear_interpolation(xvalues,yvalues), xvals[end])
+end   
+
+function n_polytrope_initial_condition!(sm::StellarModel, M::Real, R::Real; initial_dt=100 * SECYEAR)
+    (θ_n, ξ_1) = get_θn_and_ξ1(n) #get θ_n from numerical integration and interpolation
+    logdqs = get_logdq.(1:(sm.nz), sm.nz, -3.0, 0.0, 100)
+    dqs = 10 .^ logdqs
+    dqs = dqs ./ sum(dqs)
+    dms = dqs .* M
+    m_face = cumsum(dms)
+    m_cell = cumsum(dms)
+    # correct m_center
+    for i = 1:(sm.nz)
+        if i == 1
+            m_cell[i] = 0
+        elseif i != sm.nz
+            m_cell[i] = m_cell[i] - 0.5 * dms[i]
+        end
+    end
+
+    #n = 1  # n = 1 polytrope after all...
+    #rn = R / π  # ξ is defined as r/rn, where rn^2=(n+1)Pc/(4π G ρc^2)
+
+    ρc = M / (4π * rn^3 * (-π^2 * ForwardDiff.derivative(θ_n, π)))
+    Pc = 4π * CGRAV * rn^2 * ρc^2 / (n + 1)
+
+    ξ_cell = zeros(sm.nz)
+    ξ_face = zeros(sm.nz)
+    function mfunc(ξ, m)
+        return m - 4π * rn^3 * ρc * (-(-sin(ξ) + cos(ξ) * ξ))
+    end
+
+    for i = 1:(sm.nz)
+        if i == 1
+            ξ_cell[i] = 0
+        elseif i == sm.nz
+            ξ_cell[i] = π
+        else
+            mfunc_anon = ξ -> mfunc(ξ, m_cell[i])
+            ξ_cell[i] = find_zero(mfunc_anon, (0, π), Bisection())
+        end
+        if i == sm.nz
+            ξ_face[i] = π
+        else
+            mfunc_anon = ξ -> mfunc(ξ, m_face[i])
+            ξ_face[i] = find_zero(mfunc_anon, (0, π), Bisection())
+        end
+    end
+
+    # set radii, pressure and temperature, assuming ideal gas without Prad
+    for i = 1:(sm.nz)
+        μ = 0.5
+        XH = 1.0
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnr]] = log(rn * ξ_face[i])
+        if i > 1
+            P = Pc * (θ_n(ξ_cell[i]))^(n + 1)
+            ρ = ρc * (θ_n(ξ_cell[i]))^(n)
+        else
+            P = Pc
+            ρ = ρc
+        end
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnP]] = log(P)
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnT]] = log(P * μ / (CGAS * ρ))
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:H1]] = 1.0
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:He4]] = 0
+    end
+
+    # set m and dm
+    sm.mstar = M
+    sm.dm = dms
+    sm.m = m_face
+
+    # set luminosity
+    for i = 1:(sm.nz - 1)
+        μ = 0.5
+        Pface = Pc * (θ_n(ξ_face[i]))^(n + 1)
+        ρface = ρc * (θ_n(ξ_face[i]))^(n)
+        Tface = Pface * μ / (CGAS * ρface)
+        dlnT = sm.ind_vars[(i) * sm.nvars + sm.vari[:lnT]] - sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnT]]
+        dlnP = sm.ind_vars[(i) * sm.nvars + sm.vari[:lnP]] - sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lnP]]
+        κ = 0.2
+        sm.ind_vars[(i - 1) * sm.nvars + sm.vari[:lum]] = (dlnT / dlnP) *
+                                                          (16π * CRAD * CLIGHT * CGRAV * m_face[i] * Tface^4) /
+                                                          (3κ * Pface * LSUN)
+    end
+
+    # special cases, just copy values at edges
+    sm.ind_vars[(sm.nz - 1) * sm.nvars + sm.vari[:lnP]] = sm.ind_vars[(sm.nz - 2) * sm.nvars + sm.vari[:lnP]]
+    sm.ind_vars[(sm.nz - 1) * sm.nvars + sm.vari[:lnT]] = sm.ind_vars[(sm.nz - 2) * sm.nvars + sm.vari[:lnT]]
+    sm.ind_vars[(sm.nz - 1) * sm.nvars + sm.vari[:lum]] = sm.ind_vars[(sm.nz - 2) * sm.nvars + sm.vari[:lum]]
+
+    sm.time = 0.0
+    sm.dt = initial_dt
+    sm.model_number = 0
+end
+
