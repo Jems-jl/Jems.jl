c725900cf11a6a5f74431576ff8180def53d6d57
Matthias Fabry <matthias.fabry@kuleuven.be>
2023-11-22 09:13:07 +0100

    turn plotting back off

--- src/Plotting/Init.jl
+++ src/Plotting/Init.jl
@@ -18,7 +18,7 @@ function init_plots!(sm::StellarModel)
 
     GLMakie.set_theme!(basic_theme)
     GLMakie.activate!()
-    # GLMakie.set_window_config!(; float=true)  # place windows on top
+    GLMakie.set_window_config!(; float=true)  # place windows on top
 
     # create figure/axes objects
     init_figure!(sm)
