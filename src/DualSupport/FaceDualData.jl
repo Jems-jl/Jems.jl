2aea119edd93da335480353d52c322cad53bc918
Pablo Marchant <pablo.marchant@kuleuven.be>
2024-02-14 21:53:47 +0100

    Add infrastructure for face duals

---
+++ src/DualSupport/FaceDualData.jl
@@ -0,0 +1,56 @@
+export FaceDualData, update_face_dual_data_value!, update_face_dual_data!,
+        get_face_dual, get_m1_dual, get_00_dual
+
+struct FaceDualData{TWONVARSP1, THREENVARSP1, TNUMBER}
+    diff_cache_face::StarDiffCache{TWONVARSP1, TNUMBER}
+    diff_cache_m1::StarDiffCache{THREENVARSP1, TNUMBER}
+    diff_cache_00::StarDiffCache{THREENVARSP1, TNUMBER}
+end
+
+function FaceDualData(nvars::Int, ::Type{TNUMBER}) where{TNUMBER}
+    diff_cache_face = StarDiffCache(2*nvars, TNUMBER)
+    diff_cache_m1 = StarDiffCache(3*nvars, TNUMBER)
+    diff_cache_00 = StarDiffCache(3*nvars, TNUMBER)
+    fd = FaceDualData{2*nvars+1, 3*nvars+1, TNUMBER}(diff_cache_face, 
+                                diff_cache_00, diff_cache_m1)
+    return fd
+end
+
+function Base.zero(::Type{FaceDualData{SIZE1,SIZE2,TNUMBER}}) where {SIZE1, SIZE2, TNUMBER}
+    return FaceDualData((SIZE1-1)÷2, TNUMBER)
+end
+
+function Base.convert(::Type{FaceDualData{SIZE1, SIZE2, TN1}}, x::TN2) where {SIZE1, SIZE2, TN1<:Number, TN2<:Number} 
+    cd = zero(FaceDualData{SIZE1,SIZE2,TN1})
+    update_face_dual_data_value!(cd, x)
+    return cd
+end
+
+function update_face_dual_data_value!(fd::FaceDualData, value)
+    fd.diff_cache_face.dual_data[1] = value
+    fd.diff_cache_m1.dual_data[1] = value
+    fd.diff_cache_00.dual_data[1] = value
+end
+
+function update_face_dual_data!(fd::FaceDualData{SIZE1, SIZE2, TNUMBER}, dual::TDSC) where {SIZE1, SIZE2, TNUMBER, TDSC}
+    update_face_dual_data_value!(fd, dual.value)
+    twonvars = (SIZE1-1)
+    nvars = twonvars÷2
+    for i in 1:twonvars
+        fd.diff_cache_face.dual_data[1+i] = dual.partials[i]
+        fd.diff_cache_m1.dual_data[1+i] = dual.partials[i]
+        fd.diff_cache_00.dual_data[1+nvars+i] = dual.partials[i]
+    end
+end
+
+function get_face_dual(fd::FaceDualData)
+    return get_dual(fd.diff_cache_face)
+end
+
+function get_m1_dual(fd::FaceDualData)
+    return get_dual(fd.diff_cache_m1)
+end
+
+function get_00_dual(fd::FaceDualData)
+    return get_dual(fd.diff_cache_00)
+end
