4b8cbc3354de00345c005ea356c25f998c09b9a4
Digonto Rahman <digonto.rahman@student.kuleuven.be>
2024-02-21 12:49:14 +0100

    added Xrange - switching back to abundance now to get the posibility of adding different X

--- examples/MassLuminosity.jl
+++ examples/MassLuminosity.jl
@@ -23,94 +23,97 @@ basic_theme = Theme(fonts=(regular=texfont(:text), bold=texfont(:bold),
                           xticklabelsize=35, yticklabelsize=35, xticksmirrored=true, yticksmirrored=true),
                     Legend=(patchsize=(70, 10), framevisible=false, patchlabelgap=20, rowgap=10))
 set_theme!(basic_theme)
-
 ##
 
 massrange = (0.5:0.5:6)
-luminosity = []
+Xrange = (0.1:0.1:0.8)
+luminosities = zeros(length(massrange), length(Xrange))
 
-for mass in massrange
+for (i,mass) in enumerate(massrange)
     println(" ---------------------------------------------------------------------------------")
     println(" ---------------------------------------------------------------------------------")
     println("Mass = $mass")
 
-    ## Model creation
+    for (j,X) in enumerate(Xrange)
+        println("mass = $mass , X = $X")
+        ## Model creation
 
-    varnames = [:lnρ, :lnT, :lnr, :lum]
-    structure_equations = [Evolution.equationHSE, Evolution.equationT,
-                           Evolution.equationContinuity, Evolution.equationLuminosity]
-    remesh_split_functions = [StellarModels.split_lnr_lnρ, StellarModels.split_lum,
-                              StellarModels.split_lnT, StellarModels.split_xa]
-    net = NuclearNetwork([:H1,:He4,:C12, :N14, :O16], [(:kipp_rates, :kipp_pp), (:kipp_rates, :kipp_cno)])
-    nz = 1000
-    nextra = 100
-    eos = EOS.IdealEOS(false)
-    opacity = Opacity.SimpleElectronScatteringOpacity()
-    sm = StellarModel(varnames, structure_equations, nz, nextra,
-                      remesh_split_functions, net, eos, opacity);
+        varnames = [:lnρ, :lnT, :lnr, :lum]
+        structure_equations = [Evolution.equationHSE, Evolution.equationT,
+                               Evolution.equationContinuity, Evolution.equationLuminosity]
+        remesh_split_functions = [StellarModels.split_lnr_lnρ, StellarModels.split_lum,
+                                  StellarModels.split_lnT, StellarModels.split_xa]
+        net = NuclearNetwork([:H1,:He4,:C12, :N14, :O16], [(:kipp_rates, :kipp_pp), (:kipp_rates, :kipp_cno)])
+        nz = 1000
+        nextra = 100
+        eos = EOS.IdealEOS(false)
+        opacity = Opacity.SimpleElectronScatteringOpacity()
+        sm = StellarModel(varnames, structure_equations, nz, nextra,
+                          remesh_split_functions, net, eos, opacity);
 
-    ## Initialize StellarModel and evaluate equations and jacobian
-    n=3
-    StellarModels.n_polytrope_initial_condition!(n, sm, mass*MSUN, 100 * RSUN; initial_dt=10 * SECYEAR)
-    Evolution.set_step_info!(sm, sm.esi)
-    Evolution.cycle_step_info!(sm);
-    Evolution.set_step_info!(sm, sm.ssi)
-    Evolution.eval_jacobian_eqs!(sm)
+        ## Initialize StellarModel and evaluate equations and jacobian
+        n=3
+        StellarModels.n_polytrope_initial_condition!(n, sm, mass*MSUN, 100 * RSUN; initial_dt=10 * SECYEAR)
+        Evolution.set_step_info!(sm, sm.esi)
+        Evolution.cycle_step_info!(sm);
+        Evolution.set_step_info!(sm, sm.ssi)
+        Evolution.eval_jacobian_eqs!(sm)
 
-    ## Evolving our model
+        ## Evolving our model
 
-    open("example_options.toml", "w") do file
-        write(file,
-              """
-              [remesh]
-              do_remesh = true
+        open("example_options.toml", "w") do file
+            write(file,
+                  """
+                  [remesh]
+                  do_remesh = true
 
-              [solver]
-              newton_max_iter_first_step = 1000
-              newton_max_iter = 200
+                  [solver]
+                  newton_max_iter_first_step = 1000
+                  newton_max_iter = 200
 
-              [timestep]
-              dt_max_increase = 10.0
-              delta_R_limit = 0.01
-              delta_Tc_limit = 0.01
+                  [timestep]
+                  dt_max_increase = 10.0
+                  delta_R_limit = 0.01
+                  delta_Tc_limit = 0.01
 
-              [termination]
-              max_model_number = 300
-              max_center_T = 4e7
+                  [termination]
+                  max_model_number = 300
+                  max_center_T = 4e7
 
-              [plotting]
-              do_plotting = false
-              wait_at_termination = false
-              plotting_interval = 1
+                  [plotting]
+                  do_plotting = false
+                  wait_at_termination = false
+                  plotting_interval = 1
 
-              window_specs = ["HR", "profile", "history"]
-              window_layouts = [[1, 1],  # arrangement of plots
-                                [2, 1],
-                                [3, 1]
-                                ]
+                  window_specs = ["HR", "profile", "history"]
+                  window_layouts = [[1, 1],  # arrangement of plots
+                                    [2, 1],
+                                    [3, 1]
+                                    ]
 
-              profile_xaxis = 'mass'
-              profile_yaxes = ['log10_T']
-              profile_alt_yaxes = ['X','Y']
+                  profile_xaxis = 'mass'
+                  profile_yaxes = ['log10_T']
+                  profile_alt_yaxes = ['X','Y']
 
-              history_xaxis = 'star_age'
-              history_yaxes = ['R_surf']
-              history_alt_yaxes = ['T_center']
+                  history_xaxis = 'star_age'
+                  history_yaxes = ['R_surf']
+                  history_alt_yaxes = ['T_center']
 
-              [io]
-              profile_interval = 50
-              terminal_header_interval = 100
-              terminal_info_interval = 100
+                  [io]
+                  profile_interval = 50
+                  terminal_header_interval = 100
+                  terminal_info_interval = 100
 
-              """)
+                  """)
+        end
+        StellarModels.set_options!(sm.opt, "./example_options.toml")
+        rm(sm.opt.io.hdf5_history_filename; force=true)
+        rm(sm.opt.io.hdf5_profile_filename; force=true)
+        StellarModels.n_polytrope_initial_condition!(n, sm, mass*MSUN, 100 * RSUN; initial_dt=1000 * SECYEAR)
+        @time sm = Evolution.do_evolution_loop(sm);
+        history = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+        push!(luminosity, history[!, "L_surf"][end])
     end
-    StellarModels.set_options!(sm.opt, "./example_options.toml")
-    rm(sm.opt.io.hdf5_history_filename; force=true)
-    rm(sm.opt.io.hdf5_profile_filename; force=true)
-    StellarModels.n_polytrope_initial_condition!(n, sm, mass*MSUN, 100 * RSUN; initial_dt=1000 * SECYEAR)
-    @time sm = Evolution.do_evolution_loop(sm);
-    history = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
-    push!(luminosity, history[!, "L_surf"][end])
 
 end
 ##
