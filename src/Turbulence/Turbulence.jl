56c0bcf018abcd51f23573031a55382427b3702d
Matthias Fabry <matthias.fabry@kuleuven.be>
2024-02-26 13:43:52 +0100

    fix typing

--- src/Turbulence/Turbulence.jl
+++ src/Turbulence/Turbulence.jl
@@ -14,11 +14,11 @@ Abstract supertype from which all turbulence models must derive, _ie_:
 abstract type AbstractTurb end
 
 """
-    mutable struct TurbResults{T1<:Real}
+    mutable struct TurbResults{T1}
 
 Structure that holds various results from the evaluation of the turbulence model of a certain cell.
 """
-@kwdef mutable struct TurbResults{T1<:Real}
+@kwdef mutable struct TurbResults{T1}
     ∇::T1 = 0  # Actual temperature gradient d lnT/ d lnP
     ∇ᵣ::T1 = 0  # Radiative temperature gradient
     v_turb::T1 = 0  # velocity of turbulent motion
