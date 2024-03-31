885a18432a9a4fa63ef78f7aa4ecb32c67678932
Matthias Fabry <matthias.fabry@kuleuven.be>
2024-03-31 22:14:50 +0200

    rates need not know about eos

--- src/ReactionRates/ToyRates.jl
+++ src/ReactionRates/ToyRates.jl
@@ -1,4 +1,4 @@
-struct ToyReactionRate{TT<:Real}<:AbstractReactionRate
+struct ToyReactionRate{TT<:Real} <: AbstractReactionRate
     name::Symbol
     iso_in::Vector{Symbol}
     num_iso_in::Vector{Int64}
@@ -7,25 +7,22 @@ struct ToyReactionRate{TT<:Real}<:AbstractReactionRate
     Qvalue::TT
 end
 
-
 reaction_list[:toy_rates] = Dict(
     :toy_pp => ToyReactionRate(:toy_pp, [:H1], [4], [:He4], [1],
-        ((4 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He4].mass) * AMU * CLIGHT^2)),
+                               ((4 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He4].mass) * AMU * CLIGHT^2)),
     :toy_cno => ToyReactionRate(:toy_cno, [:H1], [4], [:He4], [1],
-        ((4 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He4].mass) * AMU * CLIGHT^2)),
+                                ((4 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He4].mass) * AMU * CLIGHT^2)),
 )
 
-
-function get_reaction_rate(reaction::ToyReactionRate, eos00::EOSResults{TT}, xa::AbstractVector{TT}, xa_index::Dict{Symbol,Int})::TT where{TT}
-    if reaction.name == :toy_pp # Taken from Onno Pols lectures with arbitrary pre-factor
-        ϵnuc = 0.1 * xa[xa_index[:H1]]^2 * eos00.ρ * (eos00.T / 1e6)^4
+function get_reaction_rate(reaction::ToyReactionRate, T::T1, ρ::T2, xa::AbstractVector{TT},
+                           xa_index::Dict{Symbol,Int})::TT where {TT,T1,T2}
+    if reaction.name == :toy_pp  # Taken from Onno Pols' lectures with arbitrary pre-factor
+        ϵnuc = 0.1 * xa[xa_index[:H1]]^2 * ρ * (T / 1e6)^4
         return ϵnuc / reaction.Qvalue
-    elseif reaction.name == :toy_cno # Taken from Onno Pols lectures with arbitrary pre-factor
-        ϵnuc = 0.1 * xa[xa_index[:H1]]^2 * eos00.ρ * (eos00.T / 1e7)^18
+    elseif reaction.name == :toy_cno  # Taken from Onno Pols' lectures with arbitrary pre-factor
+        ϵnuc = 0.1 * xa[xa_index[:H1]]^2 * ρ * (T / 1e7)^18
         return ϵnuc / reaction.Qvalue
     else
         throw(ArgumentError("No method to compute rate for $(reaction.name)"))
     end
 end
-
-
