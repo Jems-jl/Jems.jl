30c1b2e1e4e224960df47c0568c71c759f289f9c
Eva Kuipers <eva.kuipers@student.kuleuven.be>
2024-04-05 11:29:05 +0200

    First plotting tests comparing 3 different type of rates and masses

---
+++ Testing_Eva/NuclearBurning_MakeFiles.jl
@@ -0,0 +1,446 @@
+#=
+# NuclearBurning.jl
+
+This notebook provides a simple example of a star with simplified microphysics undergoing nuclear burning.
+Import all necessary Jems modules. We will also do some benchmarks, so we import BenchmarkTools as well.
+=#
+using BenchmarkTools
+using Jems.Chem
+using Jems.Constants
+using Jems.EOS
+using Jems.Opacity
+using Jems.NuclearNetworks
+using Jems.Turbulence
+using Jems.StellarModels
+using Jems.Evolution
+using Jems.ReactionRates
+
+##
+#=
+### Model creation
+
+We start by creating the stellar model. In this example we consider a model with 6 independent variables, two of which
+correspond to composition. The independent variables here are $\ln(P)$, $\ln(T)$, $\ln(r)$, the luminosity $L$ and the
+mass fractions of Hydrogen and Helium.
+
+The Evolution module has pre-defined equations corresponding to these variables, which we provide here. For now, only a
+simple (fully ionized) ideal gas law EOS is available. Similarly, only a simple simple electron scattering opacity equal
+to $\kappa=0.2(1+X)\;[\mathrm{cm^2\;g^{-1}}]$ is available.
+=#
+
+varnames = [:lnρ, :lnT, :lnr, :lum]
+structure_equations = [Evolution.equationHSE, Evolution.equationT,
+                       Evolution.equationContinuity, Evolution.equationLuminosity]
+remesh_split_functions = [StellarModels.split_lnr_lnρ, StellarModels.split_lum,
+                          StellarModels.split_lnT, StellarModels.split_xa]
+net = NuclearNetwork(   [:H1, :D2, :He3, :He4,
+                        :Be7, :Li7, :B8,
+                        :C12,   :C13,   
+                        :N13,   :N14,   :N15,
+                        :O14,   :O15,   :O16,   :O17,   :O18,   
+                        :F17,   :F18,   :F19,   
+                        ],
+
+                        [
+                        # PP I
+                        (:jina_rates, :H1_H1_to_D2_betplus_w_x_0),
+                        (:jina_rates, :H1_H1_to_D2_xxec_w_x_0),
+                        (:jina_rates, :H1_D2_to_He3_de04_n_x_0),
+                        # (:jina_rates, :H1_D2_to_He3_de04_x_x_0),
+                        (:jina_rates, :He3_He3_to_H1_H1_He4_nacr_n_x_0),
+                        # PP II
+                        (:jina_rates, :He4_He3_to_Be7_cd08_n_x_0),
+                        (:jina_rates, :He4_He3_to_Be7_cd08_n_x_1),
+                        (:jina_rates, :Be7_to_Li7_xxec_w_x_0),
+                        (:jina_rates, :H1_Li7_to_He4_He4_de04_x_x_0),
+                        (:jina_rates, :H1_Li7_to_He4_He4_de04_r_x_0),
+                        # (:jina_rates, :H1_Li7_to_He4_He4_de04_x_x_1),
+                        # (:jina_rates, :H1_Li7_to_He4_He4_de04_r_x_1),
+                        # PP III
+                        (:jina_rates, :H1_Be7_to_B8_nacr_r_x_0),
+                        (:jina_rates, :H1_Be7_to_B8_nacr_n_x_0),
+                        (:jina_rates, :B8_to_He4_He4_wc12_w_x_0),
+                        # PP IV
+                        (:jina_rates, :H1_He3_to_He4_betplus_w_x_0),
+                        
+                        # CNO Cycle 1
+
+                        (:jina_rates, :H1_C12_to_N13_ls09_r_x_0),
+                        # (:jina_rates, :H1_C12_to_N13_ls09_n_x_0),
+
+                        (:jina_rates, :N13_to_C13_wc12_w_x_0),
+
+                        (:jina_rates, :H1_C13_to_N14_nacr_r_x_0),
+                        (:jina_rates, :H1_C13_to_N14_nacr_r_x_1),
+                        # (:jina_rates, :H1_C13_to_N14_nacr_n_x_0),
+
+                        (:jina_rates, :H1_N14_to_O15_im05_r_x_0),
+                        # (:jina_rates, :H1_N14_to_O15_im05_n_x_0),
+                        # (:jina_rates, :H1_N14_to_O15_im05_n_x_1),
+                        (:jina_rates, :H1_N14_to_O15_im05_r_x_1),
+
+                        (:jina_rates, :O15_to_N15_wc12_w_x_0),
+
+                        (:jina_rates, :H1_N15_to_He4_C12_nacr_r_x_0),
+                        # (:jina_rates, :H1_N15_to_He4_C12_nacr_r_x_1),
+                        # (:jina_rates, :H1_N15_to_He4_C12_nacr_r_x_2),
+                        # (:jina_rates, :H1_N15_to_He4_C12_nacr_n_x_0),
+
+                        # CNO Cycle 2
+
+                        (:jina_rates, :H1_N14_to_O15_im05_r_x_0),
+                        # (:jina_rates, :H1_N14_to_O15_im05_n_x_0),
+                        # (:jina_rates, :H1_N14_to_O15_im05_n_x_1),
+                        # (:jina_rates, :H1_N14_to_O15_im05_r_x_1),
+
+                        (:jina_rates, :O15_to_N15_wc12_w_x_0),
+
+                        (:jina_rates, :H1_N15_to_O16_li10_r_x_0),
+                        # (:jina_rates, :H1_N15_to_O16_li10_r_x_1),
+                        # (:jina_rates, :H1_N15_to_O16_li10_n_x_0),
+
+                        (:jina_rates, :H1_O16_to_F17_ia08_n_x_0),
+
+                        (:jina_rates, :F17_to_O17_wc12_w_x_0),
+
+                        (:jina_rates, :H1_O17_to_He4_N14_il10_r_x_0),
+                        # (:jina_rates, :H1_O17_to_He4_N14_il10_r_x_1),
+                        # (:jina_rates, :H1_O17_to_He4_N14_il10_r_x_2),
+                        # (:jina_rates, :H1_O17_to_He4_N14_il10_n_x_0),
+
+                        # CNO Cycle 3
+
+                        (:jina_rates, :H1_N15_to_O16_li10_r_x_0),
+                        # (:jina_rates, :H1_N15_to_O16_li10_r_x_1),
+                        # (:jina_rates, :H1_N15_to_O16_li10_n_x_0),
+
+                        (:jina_rates, :H1_O16_to_F17_ia08_n_x_0),
+
+                        (:jina_rates, :F17_to_O17_wc12_w_x_0),
+
+                        # (:jina_rates, :H1_O17_to_F18_il10_r_x_0),
+                        # (:jina_rates, :H1_O17_to_F18_il10_r_x_1),
+                        (:jina_rates, :H1_O17_to_F18_il10_n_x_0),
+
+                        (:jina_rates, :F18_to_O18_wc12_w_x_0),
+
+                        (:jina_rates, :H1_O18_to_He4_N15_il10_n_x_0),
+                        # (:jina_rates, :H1_O18_to_He4_N15_il10_r_x_0),
+                        # (:jina_rates, :H1_O18_to_He4_N15_il10_r_x_1),
+                        # (:jina_rates, :H1_O18_to_He4_N15_il10_r_x_2),
+
+                        # CNO Cycle 4
+
+                        (:jina_rates, :H1_O16_to_F17_ia08_n_x_0),
+
+                        (:jina_rates, :F17_to_O17_wc12_w_x_0),
+
+                        (:jina_rates, :H1_O17_to_F18_il10_r_x_0),
+                        # (:jina_rates, :H1_O17_to_F18_il10_r_x_1),
+                        # (:jina_rates, :H1_O17_to_F18_il10_n_x_0),
+
+                        (:jina_rates, :F18_to_O18_wc12_w_x_0),
+
+                        (:jina_rates, :H1_O18_to_F19_il10_r_x_0),
+                        # (:jina_rates, :H1_O18_to_F19_il10_r_x_1),
+                        # (:jina_rates, :H1_O18_to_F19_il10_r_x_2),
+                        # (:jina_rates, :H1_O18_to_F19_il10_n_x_0),
+
+                        (:jina_rates, :H1_F19_to_He4_O16_nacr_r_x_0),
+                        # (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_0),
+                        # (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_1),
+                        # (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_2),
+                        # (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_3),
+                        
+                        
+                        
+                        ])
+nz = 1000
+nextra = 100
+eos = EOS.IdealEOS(true)
+opacity = Opacity.SimpleElectronScatteringOpacity()
+turbulence = Turbulence.BasicMLT(1.0)
+sm = StellarModel(varnames, structure_equations, nz, nextra, remesh_split_functions, net, eos, opacity, turbulence);
+
+##
+#=
+### Initialize StellarModel and evaluate equations and jacobian
+
+We do not have a working initial condition yet. We require pressure, temperature profiles. One simple available initial
+condition is that of an n=1 polytrope. This sets the pressure and density and computes the temperature from the EOS. The
+luminosity is initialized by assuming pure radiative transport for the temperature gradient produced by the polytrope.
+
+The normal evolution loop will store the information at the end of the step into an attribute of type `StellarStepInfo`,
+stored at `sm.esi` (_end step info_). After initializing our polytrope we can mimic that behavior by calling 
+`set_end_step_info!(sm)`. We then 'cycle' this info into the information of a hypothetical previous step with
+`cycle_step_info`, so now `sm.psi` contains our initial condition. Finally we call `set_start_step_info` to use `sm.psi`
+(_previous step info_) to populate the information needed before the Newton solver in `sm.ssi` (_start step info_).
+At last we are in position to evaluate the equations and compute the Jacobian.
+=#
+n = 3
+StellarModels.n_polytrope_initial_condition!(n, sm, nz, 0.7154,0.0142,0.0003,Chem.abundance_lists[:ASG_09],MSUN, 100 * RSUN; initial_dt=10 * SECYEAR)
+StellarModels.evaluate_stellar_model_properties!(sm, sm.props)
+Evolution.cycle_props!(sm);
+StellarModels.copy_scalar_properties!(sm.start_step_props, sm.prv_step_props)
+StellarModels.copy_mesh_properties!(sm, sm.start_step_props, sm.prv_step_props)  # or do StellarModels.remesher!(sm);
+StellarModels.evaluate_stellar_model_properties!(sm, sm.start_step_props)
+StellarModels.copy_scalar_properties!(sm.props, sm.start_step_props)
+StellarModels.copy_mesh_properties!(sm, sm.props, sm.start_step_props)
+
+##
+#=
+### Benchmarking
+
+The previous code leaves everything ready to solve the linearized system.
+For now we make use of a the serial Thomas algorithm for tridiagonal block matrices.
+We first show how long it takes to evaluate the Jacobian matrix. This requires two
+steps, the first is to evaluate properties across the model (for example, the EOS)
+and then evaluate all differential equations.
+=#
+@benchmark begin
+    StellarModels.evaluate_stellar_model_properties!($sm, $sm.props)
+    Evolution.eval_jacobian_eqs!($sm)
+end
+
+##
+#=
+
+To get an idea of how much a complete iteration of the solver takes, we need to benchmark
+both the calculation of the Jacobian and the matrix solver. This is because the matrix solver
+is destructive, as it uses the allocated Jacobian to store intermediate results. The time it takes
+to run only the matrix solver can be determined by substracting the previous benchmark from this one.
+=#
+
+@benchmark begin
+    StellarModels.evaluate_stellar_model_properties!($sm, $sm.props)
+    Evolution.eval_jacobian_eqs!($sm)
+    Evolution.thomas_algorithm!($sm)
+end
+
+##
+#=
+### Evolving our model
+
+We can now evolve our star! We will initiate a $1M_\odot$ star with a radius of $100R_\odot$ using an n=1 polytrope (it
+would be much better to use n=3 or n=3/2 polytropes, for now I only use this because there is a simple analytical
+solution). The star is expected to contract until it ignites hydrogen. We set a few options for the simulation with a
+toml file, which we generate dynamically. These simulation should complete in about a thousand steps once it reaches the
+`max_center_T` limit.
+
+Output is stored in HDF5 files, and easy to use functions are provided with the Evolution module to turn these HDF5
+files into DataFrame objects. HDF5 output is compressed by default.
+=#
+open("example_options.toml", "w") do file
+    write(file,
+          """
+          [remesh]
+          do_remesh = true
+
+          [solver]
+          newton_max_iter_first_step = 1000
+          initial_model_scale_max_correction = 0.5
+          newton_max_iter = 30
+          scale_max_correction = 0.1
+          report_solver_progress = false
+
+          [timestep]
+          dt_max_increase = 1.5
+          delta_R_limit = 0.01
+          delta_Tc_limit = 0.01
+          delta_Xc_limit = 0.005
+
+          [termination]
+          max_model_number = 2000
+          max_center_T = 1e8
+
+          [plotting]
+          do_plotting = true
+          wait_at_termination = false
+          plotting_interval = 1
+
+          window_specs = ["HR", "Kippenhahn", "profile", "TRhoProfile"]
+          window_layouts = [[1, 1],  # arrangement of plots
+                            [1, 2],
+                            [2, 1],
+                            [2, 2]
+                            ]
+
+          profile_xaxis = 'mass'
+          profile_yaxes = ['log10_T']
+          profile_alt_yaxes = ['X','Y']
+
+          history_xaxis = 'star_age'
+          history_yaxes = ['R_surf']
+          history_alt_yaxes = ['T_center']
+
+          max_log_eps = 5.0
+
+          [io]
+          profile_interval = 50
+          terminal_header_interval = 100
+          terminal_info_interval = 100
+
+          """)
+end
+StellarModels.set_options!(sm.opt, "./example_options.toml")
+rm(sm.opt.io.hdf5_history_filename; force=true)
+rm(sm.opt.io.hdf5_profile_filename; force=true)
+
+n = 3
+StellarModels.n_polytrope_initial_condition!(n, sm, nz, 0.7154, 0.0142, 0.0003, Chem.abundance_lists[:ASG_09], 
+                                            1 * MSUN, 100 * RSUN; initial_dt=10 * SECYEAR)
+@time Evolution.do_evolution_loop!(sm);
+
+##
+
+# Make the files needed
+
+using CSV
+
+# Toy Rates
+
+# history_10M_100R_Toy = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+
+# CSV.write("history_10M_100R_Toy.csv", history_10M_100R_Toy)
+
+
+# Kipp Rates
+
+# history_08M_100R = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+# history_10M_100R = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+# history_12M_100R = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+
+# CSV.write("history_08M_100R.csv", history_08M_100R)
+# CSV.write("history_10M_100R.csv", history_10M_100R)
+# CSV.write("history_12M_100R.csv", history_12M_100R)
+
+
+# Jina Rates
+
+
+history_10M_100R_Jina_Version_1 = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+
+CSV.write("history_10M_100R_Jina_Version_1.csv", history_10M_100R_Jina_Version_1)
+
+
+
+
+
+##
+
+
+#=
+### Plotting with Makie
+
+Now that our simulation is complete we can analyze the results. We make use of the Makie package for this. I'm not a fan
+of the Makie defaults, so I adjust them. I normally also adjust the fonts to be consistent with \LaTeX, but I avoid that
+here so we don't need to distribute those fonts together with Jems.
+=#
+using CairoMakie, LaTeXStrings, MathTeXEngine
+basic_theme = Theme(fonts=(regular=texfont(:text), bold=texfont(:bold),
+                           italic=texfont(:italic), bold_italic=texfont(:bolditalic)),
+                    fontsize=30, size=(1000, 750), linewidth=7,
+                    Axis=(xlabelsize=40, ylabelsize=40, titlesize=40, xgridvisible=false, ygridvisible=false,
+                          spinewidth=2.5, xminorticksvisible=true, yminorticksvisible=true, xtickalign=1, ytickalign=1,
+                          xminortickalign=1, yminortickalign=1, xticksize=14, xtickwidth=2.5, yticksize=14,
+                          ytickwidth=2.5, xminorticksize=7, xminortickwidth=2.5, yminorticksize=7, yminortickwidth=2.5,
+                          xticklabelsize=35, yticklabelsize=35, xticksmirrored=true, yticksmirrored=true),
+                    Legend=(patchsize=(70, 10), framevisible=false, patchlabelgap=20, rowgap=10))
+set_theme!(basic_theme)
+
+##
+#=
+### Compare against polytropes
+
+Below we see how the profile of the star compares to different polytropes. We make use of the facility tools to obtain
+DataFrame objects out of the hdf5 output. In particular, `get_profile_names_from_hdf5` will provide the names of all 
+profiles contained within the hdf5 file, while `get_profile_dataframe_from_hdf5` is used to obtain one DataFrame
+corresponding to one stellar profile. The animation is constructed using the `Observable` type that makie provides. Note
+that the zero points of the polytropes are arbitrary.
+=#
+profile_names = StellarModels.get_profile_names_from_hdf5("profiles.hdf5")
+
+f = Figure();
+ax = Axis(f[1, 1]; xlabel=L"\log_{10}(\rho/\mathrm{[g\;cm^{-3}]})", ylabel=L"\log_{10}(P/\mathrm{[dyn]})")
+
+pname = Observable(profile_names[1])
+
+profile = @lift(StellarModels.get_profile_dataframe_from_hdf5("profiles.hdf5", $pname))
+#To see why this is done this way, see https://docs.makie.org/stable/explanations/nodes/index.html#problems_with_synchronous_updates
+#the main issue is that remeshing changes the size of the arrays
+log10_ρ = @lift($profile[!, "log10_ρ"])
+log10_P = Observable(rand(length(log10_ρ.val)))
+
+profile_line = lines!(ax, log10_ρ, log10_P; label="real profile")
+xvals = LinRange(-13, 4, 100)
+lines!(ax, xvals, (1 + 1 / 1) .* xvals .+ 20; label="n=1")
+lines!(ax, xvals, (1 + 1 / (1.5)) .* xvals .+ 15; label="n=1.5")
+lines!(ax, xvals, (1 + 1 / 3) .* xvals .+ 15; label="n=3")
+axislegend(ax; position=:rb)
+
+model_number_str = @lift("model number=$(parse(Int,$pname))")
+profile_text = text!(ax, -10, 20; text=model_number_str)
+
+record(f, "rho_P_evolution.gif", profile_names[1:end]; framerate=2) do profile_name
+    profile = StellarModels.get_profile_dataframe_from_hdf5("profiles.hdf5", profile_name)
+    log10_P.val = profile[!, "log10_P"]
+    pname[] = profile_name
+end
+
+# ![Movie polytrope](./rho_P_evolution.gif)
+
+##
+#=
+### Check nuclear burning
+
+We see that the structure evolves towards an n=3 polytrope. Deviations near the core are due to the non-homogeneous
+composition as hydrogen is burnt. We can similarly visualize how the hydrogen mass fraction changes in the simulation.
+In here, only one frame shows the hydrogen that was burnt. To better visualize that you can adjust `profile_interval` in
+the [IO](Evolution.md##Io.jl) options (and probably adjust the framerate).
+=#
+profile_names = StellarModels.get_profile_names_from_hdf5("profiles.hdf5")
+
+f = Figure();
+ax = Axis(f[1, 1]; xlabel=L"\mathrm{Mass}\;[M_\odot]", ylabel=L"X")
+
+pname = Observable(profile_names[1])
+
+profile = @lift(StellarModels.get_profile_dataframe_from_hdf5("profiles.hdf5", $pname))
+mass = @lift($profile[!, "mass"])
+X = Observable(rand(length(mass.val)))
+model_number_str = @lift("model number=$(parse(Int,$pname))")
+
+profile_line = lines!(ax, mass, X; label="real profile")
+profile_text = text!(ax, 0.7, 0.0; text=model_number_str)
+
+record(f, "X_evolution.gif", profile_names[1:end]; framerate=2) do profile_name
+    profile = StellarModels.get_profile_dataframe_from_hdf5("profiles.hdf5", profile_name)
+    X.val = profile[!, "X"]
+    pname[] = profile_name
+end
+
+# ![Movie polytrope](./X_evolution.gif)
+
+##
+#=
+### Plot a funny HR diagram
+
+Finally, we can also access the history data of the simulation. We use this to plot a simple HR diagram. As our
+microphysics are very simplistic, and the initial condition is not very physical, this looks a bit funny!
+=#
+f = Figure();
+ax = Axis(f[1, 1]; xlabel=L"\log_{10}(T_\mathrm{eff}/[K])", ylabel=L"\log_{10}(L/L_\odot)", xreversed=true)
+history = StellarModels.get_history_dataframe_from_hdf5("history.hdf5")
+lines!(ax, log10.(history[!, "T_surf"]), log10.(history[!, "L_surf"]))
+f
+
+##
+#=
+### Perform some cleanup
+
+Internally we want to prevent storing any of the hdf5 files into our git repos, so I remove them. You can also take
+advantage of `julia` as a scripting language to post-process your simulation output in a similar way.
+=#
+rm("history.hdf5")
+rm("profiles.hdf5")
+rm("example_options.toml")
---
+++ Testing_Eva/Plotting.jl
@@ -0,0 +1,82 @@
+
+# This script is for plotting the values that are obtained for the different stellar models
+
+# Load in the dataframes that are saved as csv files
+
+using CSV
+using DataFrames
+
+# Kipp Rates
+
+star_12M = CSV.read("history_12M_100R.csv", DataFrame)
+star_10M = CSV.read("history_10M_100R.csv", DataFrame)
+star_08M = CSV.read("history_08M_100R.csv", DataFrame)
+
+L_surf_star_12 = log10.(star_12M.L_surf)
+L_surf_star_10 = log10.(star_10M.L_surf)
+L_surf_star_08 = log10.(star_08M.L_surf)
+
+T_surf_star_12 = log10.(star_12M.T_surf)
+T_surf_star_10 = log10.(star_10M.T_surf)
+T_surf_star_08 = log10.(star_08M.T_surf)
+
+
+# Toy Rates
+
+star_10M_ToyRates = CSV.read("history_10M_100R_Toy.csv", DataFrame)
+L_surf_star_10_Toy = log10.(star_10M_ToyRates.L_surf)
+T_surf_star_10_Toy = log10.(star_10M_ToyRates.T_surf)
+
+# Jina Rates
+
+star_10M_JinaRates_V1 = CSV.read("history_10M_100R_Jina_Version_1.csv", DataFrame)
+L_surf_star_10_Jina = log10.(star_10M_JinaRates_V1.L_surf)
+T_surf_star_10_Jina = log10.(star_10M_JinaRates_V1.T_surf)
+
+
+##
+
+# Set up Makie
+
+using CairoMakie, LaTeXStrings, MathTeXEngine
+basic_theme = Theme(fonts=(regular=texfont(:text), bold=texfont(:bold),
+                           italic=texfont(:italic), bold_italic=texfont(:bolditalic)),
+                    fontsize=30, size=(1000, 750), linewidth=7,
+                    Axis=(xlabelsize=40, ylabelsize=40, titlesize=40, xgridvisible=false, ygridvisible=false,
+                          spinewidth=2.5, xminorticksvisible=true, yminorticksvisible=true, xtickalign=1, ytickalign=1,
+                          xminortickalign=1, yminortickalign=1, xticksize=14, xtickwidth=2.5, yticksize=14,
+                          ytickwidth=2.5, xminorticksize=7, xminortickwidth=2.5, yminorticksize=7, yminortickwidth=2.5,
+                          xticklabelsize=35, yticklabelsize=35, xticksmirrored=true, yticksmirrored=true),
+                    Legend=(patchsize=(70, 10), framevisible=false, patchlabelgap=20, rowgap=10))
+set_theme!(basic_theme)
+
+##
+
+# Plot HR diagram
+
+f = Figure();
+
+ax = Axis(f[1, 1]; xlabel=L"\log_{10}(T_\mathrm{eff}/[K])", ylabel=L"\log_{10}(L/L_\odot)", xreversed=true)
+
+# lines!(ax, T_surf_star_12, L_surf_star_12, color=:red, linewidth=3, label = L"1.2 M_\odot")
+lines!(ax, T_surf_star_10, L_surf_star_10, color=:purple, linewidth=3, label = L"1.0 M_\odot\text{ Kippenhahn Rates}")
+# lines!(ax, T_surf_star_08, L_surf_star_08, color=:blue, linewidth=3, label = L"0.8 M_\odot")
+
+lines!(ax, T_surf_star_10_Toy, L_surf_star_10_Toy, color=:blue, linewidth=3, label = L"1.0 M_\odot \text{ Toy Rates}")
+
+lines!(ax, T_surf_star_10_Jina, L_surf_star_10_Jina, color=:pink, linewidth=3, label = L"1.0 M_\odot \text{ Jina Rates}")
+
+xlims!(ax, (4.3, 3.0)) 
+# ylims!(ax, (ymin, ymax)) 
+
+
+axislegend(position=:rt)
+
+f
+
+save("comparison_Toy_Kipp_Jina_Rates.png", f)
+# save("comparison_ToyRates_KippRates.png", f)
+# save("comparison_masses_KippRates.png", f)
+
+
+
