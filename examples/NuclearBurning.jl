5008ed939c616333e4513c26e59170da185e7b95
Pablo Marchant <pamarca@gmail.com>
2024-04-10 18:38:32 +0200

    After playing with networks

--- examples/NuclearBurning.jl
+++ examples/NuclearBurning.jl
@@ -35,35 +35,7 @@ structure_equations = [Evolution.equationHSE, Evolution.equationT,
                        Evolution.equationConvFrac]
 remesh_split_functions = [StellarModels.split_lnr_lnρ, StellarModels.split_lum,
                           StellarModels.split_lnT, StellarModels.split_xa, StellarModels.split_convfrac]
-net = NuclearNetwork([:H1, :D2, :He3, :He4,
-                      :Be7, :Li7, :B8,
-                      :C12, :N14, :O16
-                      # :C12,   :C13,   
-                      # :N13,   :N14,   :N15,
-                      # :O14,   :O15,   :O16,   :O17,   :O18,   
-                      # :F17,   :F18,   :F19,   
-                      ],
-                     [
-                      # PP I
-                      (:jina_rates, :H1_H1_to_D2_betplus_w_x_0),
-                      (:jina_rates, :H1_D2_to_He3_de04_n_x_0),
-                      # (:jina_rates, :H1_D2_to_He3_de04_x_x_0),
-                      (:jina_rates, :He3_He3_to_H1_H1_He4_nacr_n_x_0),
-                      # PP II
-                      (:jina_rates, :He4_He3_to_Be7_cd08_n_x_0),
-                      (:jina_rates, :He4_He3_to_Be7_cd08_n_x_1),
-                      (:jina_rates, :Be7_to_Li7_xxec_w_x_0),
-                      (:jina_rates, :H1_Li7_to_He4_He4_de04_x_x_0),
-                      (:jina_rates, :H1_Li7_to_He4_He4_de04_r_x_0),
-                      # (:jina_rates, :H1_Li7_to_He4_He4_de04_x_x_1),
-                      # (:jina_rates, :H1_Li7_to_He4_He4_de04_r_x_1),
-                      # PP III
-                      (:jina_rates, :H1_Be7_to_B8_nacr_r_x_0),
-                      (:jina_rates, :H1_Be7_to_B8_nacr_n_x_0),
-                      (:jina_rates, :B8_to_He4_He4_wc12_w_x_0),
-                      # PP IV
-                      (:jina_rates, :H1_He3_to_He4_betplus_w_x_0)])
-#net = NuclearNetwork([:H1, :He4, :C12, :N14, :O16], [(:kipp_rates, :kipp_pp), (:kipp_rates, :kipp_cno)])
+net = NuclearNetwork([:H1, :He4, :C12, :N14, :O16], [(:kipp_rates, :kipp_pp), (:kipp_rates, :kipp_cno)])
 nz = 1000
 nextra = 100
 eos = EOS.IdealEOS(true)
@@ -150,14 +122,14 @@ open("example_options.toml", "w") do file
           initial_model_scale_max_correction = 0.2
           newton_max_iter = 50
           scale_max_correction = 0.1
-          report_solver_progress = false
+          report_solver_progress = true
           solver_progress_iter = 1
 
           [timestep]
           dt_max_increase = 1.5
           delta_R_limit = 0.01
           delta_Tc_limit = 0.01
-          delta_Xc_limit = 0.0025
+          delta_Xc_limit = 0.001
 
           [termination]
           max_model_number = 2000
