de3601cdac21c31fca93cc835932dbe114565421
Eva Kuipers <eva.kuipers@student.kuleuven.be>
2024-04-13 15:32:12 +0200

    Some benchmarking

--- Testing_Eva/Benchmarking_Comparison.jl
+++ Testing_Eva/Benchmarking_Comparison.jl
@@ -27,6 +27,14 @@ y4_file = BenchmarkTools.load("Kipp_RUN4_b.json"); y4 = y4_file[1]
 y5_file = BenchmarkTools.load("Kipp_RUN5_b.json"); y5 = y5_file[1]
 
 
+Kipp_file_a = BenchmarkTools.load("Kipp_RUN1_a.json"); Kipp_a = Kipp_file_a[1]
+Jina_file_a = BenchmarkTools.load("Jina_RUN1_a.json"); Jina_a = Jina_file_a[1]
+
+Kipp_file_b = BenchmarkTools.load("Kipp_RUN1_b.json"); Kipp_b = Kipp_file_b[1]
+Jina_file_b = BenchmarkTools.load("Jina_RUN1_b.json"); Jina_b = Jina_file_b[1]
+
+
+
 # Start comparing the benchmarks
 
 ##
@@ -50,7 +58,36 @@ BenchmarkTools.judge(median(y3), median(y4))
 BenchmarkTools.judge(median(y4), median(y5))
 ##
 
+
+# Compare Jina and Kipp
+
+BenchmarkTools.judge(median(Kipp_a), median(Jina_a))
+##
+BenchmarkTools.judge(median(Kipp_b), median(Jina_b))
+##
+
+
+# Set up Makie
+
+using CairoMakie, LaTeXStrings, MathTeXEngine
+basic_theme = Theme(fonts=(regular=texfont(:text), bold=texfont(:bold),
+                           italic=texfont(:italic), bold_italic=texfont(:bolditalic)),
+                    fontsize=30, size=(1000, 750), linewidth=7,
+                    Axis=(xlabelsize=40, ylabelsize=40, titlesize=40, xgridvisible=false, ygridvisible=false,
+                          spinewidth=2.5, xminorticksvisible=true, yminorticksvisible=true, xtickalign=1, ytickalign=1,
+                          xminortickalign=1, yminortickalign=1, xticksize=14, xtickwidth=2.5, yticksize=14,
+                          ytickwidth=2.5, xminorticksize=7, xminortickwidth=2.5, yminorticksize=7, yminortickwidth=2.5,
+                          xticklabelsize=35, yticklabelsize=35, xticksmirrored=true, yticksmirrored=true),
+                    Legend=(patchsize=(70, 10), framevisible=false, patchlabelgap=20, rowgap=10))
+set_theme!(basic_theme)
+
+##
+
 # PLOT
+
+f = Figure()
+
+ax1 = Axis(f[1, 1])
 
 data = x1.times
 nbins = 30
@@ -61,12 +98,17 @@ counts = [count(x -> intervals[i] <= x < intervals[i+1], data) for i in 1:nbins]
 middle_values = [(intervals[i] + intervals[i+1]) / 2 for i in 1:length(intervals)-1]
 middle_array = collect(middle_values)
 
-
-barplot(
-    middle_array, # x data
-    counts, # y data
-    color = "purple"
+barplot!(
+    middle_array,       # x data
+    counts,             # y data
+    color = :orchid1      # color
 
 )
+# vlines!([median(x1).time,
+#          median(x1).time - std(x1).time,
+#          median(x1).time + std(x1).time],
+#          linewidth = 3,
+#          color = :blue)
 
 
+f
--- Testing_Eva/NuclearBurning_Benchmarking.jl
+++ Testing_Eva/NuclearBurning_Benchmarking.jl
@@ -34,9 +34,128 @@ structure_equations = [Evolution.equationHSE, Evolution.equationT,
 remesh_split_functions = [StellarModels.split_lnr_lnρ, StellarModels.split_lum,
                           StellarModels.split_lnT, StellarModels.split_xa]
 
-net = NuclearNetwork([:H1, :D2, :He3, :He4, :Li7, :Be8, :C12, :N14, :O16],
-                     [(:kipp_rates, :kipp_pp),
-                      (:kipp_rates, :kipp_cno)])
+                          net = NuclearNetwork(   [:H1, :D2, :He3, :He4,
+                          :Be7, :Li7, :B8,
+                          :C12,   :C13,   
+                          :N13,   :N14,   :N15,
+                          :O14,   :O15,   :O16,   :O17,   :O18,   
+                          :F17,   :F18,   :F19,   
+                          ],
+  
+                          [
+                          # PP I
+                          (:jina_rates, :H1_H1_to_D2_betplus_w_x_0),
+                          (:jina_rates, :H1_H1_to_D2_xxec_w_x_0),
+                          (:jina_rates, :H1_D2_to_He3_de04_n_x_0),
+                          (:jina_rates, :H1_D2_to_He3_de04_x_x_0),
+                          (:jina_rates, :He3_He3_to_H1_H1_He4_nacr_n_x_0),
+                          # PP II
+                          (:jina_rates, :He4_He3_to_Be7_cd08_n_x_0),
+                          (:jina_rates, :He4_He3_to_Be7_cd08_n_x_1),
+                          (:jina_rates, :Be7_to_Li7_xxec_w_x_0),
+                          (:jina_rates, :H1_Li7_to_He4_He4_de04_x_x_0),
+                          (:jina_rates, :H1_Li7_to_He4_He4_de04_r_x_0),
+                          (:jina_rates, :H1_Li7_to_He4_He4_de04_x_x_1),
+                          (:jina_rates, :H1_Li7_to_He4_He4_de04_r_x_1),
+                          # PP III
+                          (:jina_rates, :H1_Be7_to_B8_nacr_r_x_0),
+                          (:jina_rates, :H1_Be7_to_B8_nacr_n_x_0),
+                          (:jina_rates, :B8_to_He4_He4_wc12_w_x_0),
+                          # PP IV
+                          (:jina_rates, :H1_He3_to_He4_betplus_w_x_0),
+                          
+                          # CNO Cycle 1
+  
+                          (:jina_rates, :H1_C12_to_N13_ls09_r_x_0),
+                          (:jina_rates, :H1_C12_to_N13_ls09_n_x_0),
+  
+                          (:jina_rates, :N13_to_C13_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_C13_to_N14_nacr_r_x_0),
+                          (:jina_rates, :H1_C13_to_N14_nacr_r_x_1),
+                          (:jina_rates, :H1_C13_to_N14_nacr_n_x_0),
+  
+                          (:jina_rates, :H1_N14_to_O15_im05_r_x_0),
+                          (:jina_rates, :H1_N14_to_O15_im05_n_x_0),
+                          (:jina_rates, :H1_N14_to_O15_im05_n_x_1),
+                          (:jina_rates, :H1_N14_to_O15_im05_r_x_1),
+  
+                          (:jina_rates, :O15_to_N15_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_N15_to_He4_C12_nacr_r_x_0),
+                          (:jina_rates, :H1_N15_to_He4_C12_nacr_r_x_1),
+                          (:jina_rates, :H1_N15_to_He4_C12_nacr_r_x_2),
+                          (:jina_rates, :H1_N15_to_He4_C12_nacr_n_x_0),
+  
+                          # CNO Cycle 2
+  
+                          (:jina_rates, :H1_N14_to_O15_im05_r_x_0),
+                          (:jina_rates, :H1_N14_to_O15_im05_n_x_0),
+                          (:jina_rates, :H1_N14_to_O15_im05_n_x_1),
+                          (:jina_rates, :H1_N14_to_O15_im05_r_x_1),
+  
+                          (:jina_rates, :O15_to_N15_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_N15_to_O16_li10_r_x_0),
+                          (:jina_rates, :H1_N15_to_O16_li10_r_x_1),
+                          (:jina_rates, :H1_N15_to_O16_li10_n_x_0),
+  
+                          (:jina_rates, :H1_O16_to_F17_ia08_n_x_0),
+  
+                          (:jina_rates, :F17_to_O17_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_O17_to_He4_N14_il10_r_x_0),
+                          (:jina_rates, :H1_O17_to_He4_N14_il10_r_x_1),
+                          (:jina_rates, :H1_O17_to_He4_N14_il10_r_x_2),
+                          (:jina_rates, :H1_O17_to_He4_N14_il10_n_x_0),
+  
+                          # CNO Cycle 3
+  
+                          (:jina_rates, :H1_N15_to_O16_li10_r_x_0),
+                          (:jina_rates, :H1_N15_to_O16_li10_r_x_1),
+                          (:jina_rates, :H1_N15_to_O16_li10_n_x_0),
+  
+                          (:jina_rates, :H1_O16_to_F17_ia08_n_x_0),
+  
+                          (:jina_rates, :F17_to_O17_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_O17_to_F18_il10_r_x_0),
+                          (:jina_rates, :H1_O17_to_F18_il10_r_x_1),
+                          (:jina_rates, :H1_O17_to_F18_il10_n_x_0),
+  
+                          (:jina_rates, :F18_to_O18_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_O18_to_He4_N15_il10_n_x_0),
+                          (:jina_rates, :H1_O18_to_He4_N15_il10_r_x_0),
+                          (:jina_rates, :H1_O18_to_He4_N15_il10_r_x_1),
+                          (:jina_rates, :H1_O18_to_He4_N15_il10_r_x_2),
+  
+                          # CNO Cycle 4
+  
+                          (:jina_rates, :H1_O16_to_F17_ia08_n_x_0),
+  
+                          (:jina_rates, :F17_to_O17_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_O17_to_F18_il10_r_x_0),
+                          (:jina_rates, :H1_O17_to_F18_il10_r_x_1),
+                          (:jina_rates, :H1_O17_to_F18_il10_n_x_0),
+  
+                          (:jina_rates, :F18_to_O18_wc12_w_x_0),
+  
+                          (:jina_rates, :H1_O18_to_F19_il10_r_x_0),
+                          (:jina_rates, :H1_O18_to_F19_il10_r_x_1),
+                          (:jina_rates, :H1_O18_to_F19_il10_r_x_2),
+                          (:jina_rates, :H1_O18_to_F19_il10_n_x_0),
+  
+                          (:jina_rates, :H1_F19_to_He4_O16_nacr_r_x_0),
+                          (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_0),
+                          (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_1),
+                          (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_2),
+                          (:jina_rates, :H1_F19_to_He4_O16_nacr_x_x_3),
+                          
+                          
+                          
+                          ])
 
 nz = 1000
 nextra = 100
@@ -87,7 +206,7 @@ end
 
 ##
 
-BenchmarkTools.save("Kipp_RUN5_a.json", a)
+BenchmarkTools.save("Jina_RUN1_a.json", a)
 
 ##
 
@@ -110,7 +229,7 @@ end
 
 ##
 
-BenchmarkTools.save("Kipp_RUN5_b.json", b)
+BenchmarkTools.save("Jina_RUN1_b.json", b)
 
 
 ##
