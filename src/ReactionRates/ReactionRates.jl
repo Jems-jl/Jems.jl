13ec1512c7bbdd6107b4e6b85f9e728c4ab87ced
Pablo <pamarca@gmail.com>
2025-04-10 15:51:04 +0200

    Proper documentation for ReactionRates

--- src/ReactionRates/ReactionRates.jl
+++ src/ReactionRates/ReactionRates.jl
@@ -4,8 +4,49 @@ using ..Constants, ..EOS, ..Chem
 
 export ToyReactionRate, KippReactionRate
 
+"""
+    abstract type AbstractReactionRate end
+
+Reaction rates should be defined as a subtype of `AbstractReactionRate`. Any custom reaction rate needs to contain a set of fixed fields:
+
+```julia
+struct MyReactionRate{TT<:Real} <: ReactionRates.AbstractReactionRate
+    name::Symbol
+    # num_iso_in of isotopes iso_in are converted into num_iso_out of isotopes iso_out
+    iso_in::Vector{Symbol}
+    num_iso_in::Vector{Int}
+    iso_out::Vector{Symbol}
+    num_iso_out::Vector{Int}
+    Qvalue::TT  # energy released per reaction of this type (i.e. the mass defect), in erg
+end
+```
+arbitrary fields can be added here. For instance if you have a tabulated reaction rate you can add the table values or even include the data defining a specific interpolator for it.
+
+For any custom rate the following function needs to be defined
+```julia
+function get_reaction_rate(reaction::MyReactionRate, T::T1, ρ::T2, xa::AbstractVector{TT}
+    #insert your code here
+end
+```
+the function computes the specific reaction rate based on the `name` field of the reaction or done generically based on the other fields of the struct. Units are in ``\\mathrm{s^{-1}\\;g^{-1}}``
+"""
 abstract type AbstractReactionRate end
 
+"""
+    reaction_list::Dict{Symbol, Dict{Symbol,AbstractReactionRate}} = Dict()
+
+The `reaction_list` dictionary contains reaction rates that are available for construction of nuclear reaction networks. It is a dictionary of dictionaries, for instance accessing `reaction_list[:jina_rates]` will provide all available rates from JINA.
+
+You can add a new dictionary of rates to `reaction_list` by doing
+```julia
+reaction_list[:kipp_rates] = Dict(
+    :kipp_pp => MyReactionRate(:my_pp, [:H1], [2], [:H2], [1],
+                                 ((2 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He2].mass) * AMU * CLIGHT^2)),
+                                 # add whatever more rates
+                                 )
+```
+this assumes a specific form for the constructor of `MyReactionRate` which only includes the mandatory fields, you can use any custom constructor for rate definitions which have more fields than the standard ones.
+"""
 reaction_list::Dict{Symbol, Dict{Symbol,AbstractReactionRate}} = Dict()
 
 include("JinaRates.jl")
--- src/ReactionRates/ToyRates.jl
+++ src/ReactionRates/ToyRates.jl
@@ -1,3 +1,14 @@
+"""
+    struct ToyReactionRate{TT<:Real}<:ReactionRates.AbstractReactionRate
+
+
+- `name`: `Symbol` giving the name of the reaction.
+- `iso_in`: vector that contains all reactants given as symbols (e.g. `[:H1, :H2]`)
+- `num_iso_in`: number of each of the elements in `iso_in` that are used in the reaction, given as a vector of integers. For example if `iso_in` is `[:He4]` and `num_iso_in` is `[3]` it means the reaction uses three ":He4".
+- `iso_out`: Same as `iso_in` but for the products of the reaction.
+- `num_iso_out`: Same as `num_iso_in` but for the products of the reaction.
+- `Qvalue`: Q-value of the reaction (in erg), simply given by the mass difference.
+"""
 struct ToyReactionRate{TT<:Real} <: AbstractReactionRate
     name::Symbol
     iso_in::Vector{Symbol}
@@ -14,6 +25,20 @@ reaction_list[:toy_rates] = Dict(
                                 ((4 * Chem.isotope_list[:H1].mass - Chem.isotope_list[:He4].mass) * AMU * CLIGHT^2)),
 )
 
+"""
+    function get_reaction_rate(reaction::ToyReactionRate, T::T1, ρ::T2, xa::AbstractVector{TT},
+                                xa_index::Dict{Symbol,Int})::TT where {TT,T1,T2}
+
+Input:
+reaction: the reaction to evaluate for
+- `T``: the temperature in K
+- `ρ``: the density g cm^-3
+- `xa``: element mass fractions
+- `xa_index`: Dictionary containing the index of each element within `xa`
+
+Output:
+- ϵ_nuc / Qvalue, has units s^-1 g^-1
+"""
 function get_reaction_rate(reaction::ToyReactionRate, T::T1, ρ::T2, xa::AbstractVector{TT},
                            xa_index::Dict{Symbol,Int})::TT where {TT,T1,T2}
     if reaction.name == :toy_pp  # Taken from Onno Pols' lectures with arbitrary pre-factor
