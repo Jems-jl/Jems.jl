7aa74fbbc5ef4140b2d5b53fdaabc9436047f0b2
orlox <pamarca@gmail.com>
2025-11-27 09:40:24 +0100

    Improve HRPlot

--- src/Plotting/HRPlot.jl
+++ src/Plotting/HRPlot.jl
@@ -1,17 +1,22 @@
-mutable struct HRPlot{TOBS, TAXIS}
+mutable struct HRPlot{TOBS, TOBS2, TAXIS}
     logTeff::TOBS
     logL::TOBS
+    current_position::TOBS2
     axis::TAXIS
 end
 
 function HRPlot(grid_pos)
-    axis = Axis(grid_pos, xlabel="logTeff", ylabel="logL")
+    axis = Axis(grid_pos, xlabel="logTeff", ylabel="logL", xreversed=true)
     logTeff_start::Vector{Float64} = []
     logL_start::Vector{Float64} = []
     logTeff = Observable(logTeff_start)
     logL = Observable(logL_start)
     lines!(axis, logTeff, logL)
-    return HRPlot(logTeff, logL, axis)
+
+    current_position = Observable(Point(0.0,0.0))
+    scatter!(axis, current_position, marker=:circle, color=:red)
+
+    return HRPlot(logTeff, logL, current_position, axis)
 end
 
 function update_plot!(p::HRPlot, m)
@@ -19,4 +24,5 @@ function update_plot!(p::HRPlot, m)
     new_logL = log10(get_value(m.props.L[m.props.nz]))
     p.logTeff[] = push!(p.logTeff[], new_logTeff)
     p.logL[] = push!(p.logL[], new_logL)
+    p.current_position[] = Point(new_logTeff, new_logL)
 end
