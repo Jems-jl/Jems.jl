6179862df802b7516d843d9e0647a729c2261703
matthiasfabry <matthias.fabry@villanova.edu>
2026-08-05 12:44:04 -0400

    move TwoDeeThrow to examples

--- examples/TwoDeeThrow.jl
+++ examples/TwoDeeThrow.jl
@@ -1,5 +1,14 @@
+#=
+# TwoDeeThrow.jl
+
+This notebook provides a simple example using dual numbers to compute the trajectory of a projectile in two dimensions. 
+The projectile is launched with an initial velocity v0 at an angle theta0 from the horizontal. 
+The equations of motion are given by Newton's second law, and we will use the Runge-Kutta 4th order method to integrate these equations over time.
+The use of duals allows us to compute the derivatives of the final position with respect to the initial conditions, 
+which can be useful for sensitivity analysis and optimization problems.
+=#
+
 import ForwardDiff: Dual
-using NLsolve
 using GLMakie, LaTeXStrings, MathTeXEngine, Printf
 basic_theme = Theme(fonts=(regular=texfont(:text), bold=texfont(:bold),
                            italic=texfont(:italic), bold_italic=texfont(:bolditalic)),
@@ -12,11 +21,9 @@ basic_theme = Theme(fonts=(regular=texfont(:text), bold=texfont(:bold),
                     Legend=(patchsize=(50, 10), framevisible=false, patchlabelgap=10, rowgap=5, labelsize=15))
 set_theme!(basic_theme)
 GLMakie.activate!(; scalefactor=4)
-##
+## convenience functions for working with dual numbers
 to_value = x -> x.value
 first_partial = x -> x.partials[1]
-second_partial = x -> x.partials[2]
-third_partial = x -> x.partials[3]
 ## define the system of ODEs for projectile motion
 g = 1
 
@@ -32,33 +39,31 @@ x0 = 0.0
 t0 = 0.0
 theta0s = LinRange(pi/8, 3*pi/8, 100)
 dt = 0.08 # time step
-##
+## define analytical solutions for comparison
 function x_range_analytic(theta0)
     return x0 + v0^2 * cos(theta0) * sin(theta0) / g * (1 + sqrt(1 + 2*g*(y0)/v0^2/sin(theta0)^2))
 end
-
 function y_path_analytic(x, theta0)
     y0 + (x - x0) * tan(theta0) - (g / (2 * v0^2 * cos(theta0)^2)) * (x - x0)^2
 end
-y_analytic(t) = y0 + v0 * sin(theta0.value) * t - (g / 2) * t^2
-x_analytic(t) = x0 + v0 * cos(theta0.value) * t
-∂y_∂t_analytic(t) = v0 * sin(theta0.value) - g * t
-∂x_∂t_analytic(t) = v0 * cos(theta0.value)
-∂y_∂v₀_analytic(t) = sin(theta0.value) * t
-∂y_∂θ₀_analytic(t) = v0 * cos(theta0.value) * t
-∂y_∂₀_analytic(t) = 1.0
-∂x_∂v₀_analytic(t) = cos(theta0.value) * t
+y_analytic(t, theta0) = y0 + v0 * sin(theta0) * t - (g / 2) * t^2
+x_analytic(t, theta0) = x0 + v0 * cos(theta0) * t
+∂y_∂t_analytic(t, theta0) = v0 * sin(theta0) - g * t
+∂x_∂t_analytic(t, theta0) = v0 * cos(theta0)
+∂y_∂v₀_analytic(t, theta0) = sin(theta0) * t
+∂y_∂θ₀_analytic(t, theta0) = v0 * cos(theta0) * t
+∂y_∂v₀_analytic(t, theta0) = 1.0
+∂x_∂v₀_analytic(t, theta0) = cos(theta0) * t
 ∂x_∂θ₀_analytic(t, theta0) = -v0 * sin(theta0) * t
 dx_dθ₀_analytic(theta0) = 2*cos(2*theta0)
 t_end_analytic(theta0) = v0 * sin(theta0) / g * (1 + sqrt(1+ 2 * g * y0 / (v0^2 * sin(theta0)^2)))
-
-function x_path_analytic(y)
-    if y > y0 + v0^2 * sin(theta0.value)^2 / (2*g)
+function x_path_analytic(y, theta0)
+    if y > y0 + v0^2 * sin(theta0)^2 / (2*g)
         return NaN
     end
-    return x0 + v0^2 * cos(theta0.value) * sin(theta0.value) / g +
-           v0^2 * cos(theta0.value)^2 / g *
-           sqrt(tan(theta0.value)^2 + 2*g*(y0 - y)/v0^2/cos(theta0.value)^2)
+    return x0 + v0^2 * cos(theta0) * sin(theta0) / g +
+           v0^2 * cos(theta0)^2 / g *
+           sqrt(tan(theta0)^2 + 2*g*(y0 - y)/v0^2/cos(theta0)^2)
 end
 function dx_dtheta_path_analytic(y, theta0)
     if y > y0 + v0^2 * sin(theta0)^2 / (2*g)
@@ -68,7 +73,7 @@ function dx_dtheta_path_analytic(y, theta0)
     return v0^2 * cos(2*theta0) / g * (1+sq) - (y0 - y) / sq * 2 * cot(theta0)^2
 end
 
-##
+## setup arrays to store results for each initial angle
 t_arrays = []
 y_arrays = []
 x_arrays = []
@@ -76,6 +81,10 @@ vx_arrays = []
 vy_arrays = []
 
 function RK4(t, x, vx, y, vy, dt)
+    """
+    Perform a single Runge-Kutta 4th order step for the system of ODEs defined by dx_dt, dvx_dt, dy_dt, dvy_dt.
+    Returns the updated values of t, x, vx, y, vy after a time step
+    """
     t_next = t + dt
     k1_x = dx_dt(x, vx, y, vy)
     k1_vx = dvx_dt(x, vx, y, vy)
@@ -101,6 +110,10 @@ function RK4(t, x, vx, y, vy, dt)
 end
 
 function forward_euler_step(t, x, vx, y, vy, dt)
+    """
+    Perform a single forward Euler step for the system of ODEs defined by dx_dt, dvx_dt, dy_dt, dvy_dt.
+    Returns the updated values of t, x, vx, y, vy after a time step
+    """
     t_next = t + dt
     x_next = x + dx_dt(x, vx, y, vy) * dt
     vx_next = vx + dvx_dt(x, vx, y, vy) * dt
@@ -110,18 +123,16 @@ function forward_euler_step(t, x, vx, y, vy, dt)
 end
 
 for theta0 in theta0s
-    θ₀ = Dual(theta0, 1.0)
+    θ₀ = Dual(theta0, 1.0)  # make theta0 a dual number to compute derivatives with respect to it
 
     t_array = [t0]
-    y_array = [Dual(y0, 0.0)]
+    y_array = [Dual(y0, 0.0)]  # the dependent coordinates are also dual numbers to compute derivatives wrt theta0
     x_array = [Dual(x0, 0.0)]
     vx_array = [v0 * cos(θ₀)]
     vy_array = [v0 * sin(θ₀)]
 
-    ## do integration loop
-
+    # do integration loop
     for i in 1:10000
-        # Forward Euler method
         t = t_array[end]
         x = x_array[end]
         y = y_array[end]
@@ -129,14 +140,13 @@ for theta0 in theta0s
         vy = vy_array[end]
 
         t_next, x_next, vx_next, y_next, vy_next = RK4(t, x, vx, y, vy, dt)
-        # Store the results
         push!(t_array, t_next)
         push!(x_array, x_next)
         push!(y_array, y_next)
         push!(vx_array, vx_next)
         push!(vy_array, vy_next)
         
-        # Break if the object hits the ground
+        # Break if the object hits (or is in) the ground
         if y_next.value < 0.0
             break
         end
@@ -147,7 +157,7 @@ for theta0 in theta0s
     push!(vx_arrays, vx_array)
     push!(vy_arrays, vy_array)
 end
-##
+## plot the trajectories for each initial angle, along with the analytical solutions
 f = Figure();
 ax = Axis(f[1, 1])
 for i in eachindex(theta0s)
@@ -155,8 +165,7 @@ for i in eachindex(theta0s)
     lines!(ax, to_value.(x_arrays[i]), y_path_analytic.(to_value.(x_arrays[i]), theta0s[i]), linewidth=3)
 end
 f
-## define interpolation and analytical functions for comparison
-
+## define interpolation functions
 function interpolate(x, x_array, y_array)  # interpolates y_array(x_array) at an input x
     for i in eachindex(x_array)[2:end]
         if x_array[i-1] < x <= x_array[i] || x_array[i] < x <= x_array[i-1]
@@ -175,14 +184,8 @@ function slope(x, x_array, y_array)  # returns the slope of the linear interpola
     end
     return Dual(NaN, NaN)
 end
-function find_t_end(t_array, y_array)
-    return interpolate(0, y_array, t_array)
-end
-function find_x_end(x_array, y_array)
-    return interpolate(0, y_array, x_array)
-end
 
-##
+## find the x and y values at which the projectile hits the ground (y=0) for each initial angle
 xends = []
 yends = []
 x_nearests = []
@@ -240,8 +243,7 @@ linkxaxes!(ax, ax2)
 f
 save("developing/range_theta_0.png", f)
 
-##
-
+## choose the trajectory for π/4...
 x_array = x_arrays[div(length(theta0s),2)]
 y_array = y_arrays[div(length(theta0s),2)]
 t_array = t_arrays[div(length(theta0s),2)]
@@ -257,7 +259,7 @@ int_dual_label(dualx, dualy) = L"\mathcal{\hat{X}} = ({%$(write_to_string(dualx.
 xs = LinRange(to_value(x_array[end-1]), to_value(x_array[end]), 100)
 ys = interpolate.(Dual.(xs, 0), Ref(to_value.(x_array)), Ref(to_value.(y_array)))
 
-##
+## ...and plots its final points
 f = Figure();
 ax = Axis(f[1, 1], xlabel=L"x", ylabel=L"y")
 scatter!(ax, to_value.(x_array)[end-3:end], to_value.(y_array)[end-3:end], color=:blue, label=L"y(x)", markersize=10)
@@ -269,6 +271,5 @@ annotation!(ax, to_value(x_array[end]), to_value(y_array[end]), text=dual_label(
 annotation!(ax, to_value(x_array[end-1]), to_value(y_array[end-1]), text=dual_label(x_array[end-1], y_array[end-1]), align=(:left, :bottom), fontsize=15)
 annotation!(ax, to_value(x_array[end-2]), to_value(y_array[end-2]), text=dual_label(x_array[end-2], y_array[end-2]), align=(:left, :bottom), fontsize=15)
 annotation!(ax, to_value(x_array[end-3]), to_value(y_array[end-3]), text=dual_label(x_array[end-3], y_array[end-3]), align=(:left, :top), fontsize=15)
-
-
 f
+save("developing/y_x.png", f)
